cf12445c0daeb17352a89748919e9a960fd20fb0
ssrzz <ssrzz@pm.me>
2018-12-21 11:55:41 +0800

    update: make sleep time customizable

--- daemon.sh
+++ daemon.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+st=$1
+: ${st:=1800} # Default sleep time is 30 minutes 
 while :; do
 	echo "" > nohup.out
 	nohup python3 daily_submit.py &
 	pid=$!
-	sleep 1800
+	sleep $st
 	kill -9 $pid 
 done
 
 # bash test.sh &
 # pid=$!
 # echo $!
-# sleep 1
+
+# sleep_time=$1
+# : ${sleep_time:=3}
+# sleep $sleep_time
+
 # echo $pid
