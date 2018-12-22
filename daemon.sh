03565b7ec62c2dac01b9fdb01ccefa1829a41699
ssrzz <ssrzz@pm.me>
2018-12-23 00:42:19 +0800

    minor-update: login before submit

--- daemon.sh
+++ daemon.sh
@@ -4,6 +4,7 @@ st=$1
 : ${st:=1800} # Default sleep time is 30 minutes 
 while :; do
 	echo "" > nohup.out
+	ruby login.rb  # login first 
 	pkill -f "python3 daily"	
 	nohup python3 daily_submit.py &
 	sleep $st
