20d94f2791a2c7fc79ac70f9daf6cd1a53f068fb
ssrzz <ssrzz@pm.me>
2019-01-17 08:05:10 +0800

    [Hard] p381 ruby solution added

--- python_solutions/381.insert-delete-getrandom-o1-duplicates-allowed.rb
+++ python_solutions/381.insert-delete-getrandom-o1-duplicates-allowed.rb
@@ -14,85 +14,85 @@
 # Design a data structure that supports all following operations in average
 # O(1) time.
 # Note: Duplicate elements are allowed.
-# 
-# 
+#
+#
 # insert(val): Inserts an item val to the collection.
 # remove(val): Removes an item val from the collection if present.
 # getRandom: Returns a random element from current collection of elements. The
 # probability of each element being returned is linearly related to the number
 # of same value the collection contains.
-# 
-# 
-# 
+#
+#
+#
 # Example:
-# 
+#
 # // Init an empty collection.
 # RandomizedCollection collection = new RandomizedCollection();
-# 
+#
 # // Inserts 1 to the collection. Returns true as the collection did not
 # contain 1.
 # collection.insert(1);
-# 
+#
 # // Inserts another 1 to the collection. Returns false as the collection
 # contained 1. Collection now contains [1,1].
 # collection.insert(1);
-# 
+#
 # // Inserts 2 to the collection, returns true. Collection now contains
 # [1,1,2].
 # collection.insert(2);
-# 
+#
 # // getRandom should return 1 with the probability 2/3, and returns 2 with the
 # probability 1/3.
 # collection.getRandom();
-# 
+#
 # // Removes 1 from the collection, returns true. Collection now contains
 # [1,2].
 # collection.remove(1);
-# 
+#
 # // getRandom should return 1 and 2 both equally likely.
 # collection.getRandom();
-# 
-# 
+#
+#
 #
 class RandomizedCollection
+  #     Initialize your data structure here.
+  def initialize
+    @pos = {}
+    @num = []
+  end
 
-=begin
-    Initialize your data structure here.
-=end
-    def initialize()
-        
-    end
+  #     Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+  #     :type val: Integer
+  #     :rtype: Boolean
+  def insert(val)
+    res = true
+    res = false if @pos.key?(val)
+    @pos[val] = [] unless @pos.key?(val)
+    @pos[val] << @num.size
+    @num << [val, @pos[val].size - 1]
+    res
+  end
 
+  #     Removes a value from the collection. Returns true if the collection contained the specified element.
+  #     :type val: Integer
+  #     :rtype: Boolean
+  def remove(val)
+    return false if @pos.empty? || !@pos.key?(val)
 
-=begin
-    Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
-    :type val: Integer
-    :rtype: Boolean
-=end
-    def insert(val)
-        
-    end
+    lastval, lastidx = @num.last
+    @pos[lastval][lastidx] = @pos[val].last
+    @num[@pos[val].last] = @num.last
+    @num.pop
+    @pos[val].pop
+    @pos.delete(val) if @pos[val] == []
+    true
+  end
 
-
-=begin
-    Removes a value from the collection. Returns true if the collection contained the specified element.
-    :type val: Integer
-    :rtype: Boolean
-=end
-    def remove(val)
-        
-    end
-
-
-=begin
-    Get a random element from the collection.
-    :rtype: Integer
-=end
-    def get_random()
-        
-    end
-
-
+  #     Get a random element from the collection.
+  #     :rtype: Integer
+  def get_random
+    @num.sample.first
+  end
 end
 
 # Your RandomizedCollection object will be instantiated and called as such:
