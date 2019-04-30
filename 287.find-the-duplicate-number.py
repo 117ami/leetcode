36d524d981b30e6353dcff001b9af9a2f9bbed0b
ssrzz <ssrzz@pm.me>
2019-04-30 19:10:05 +0800

    [Medium] p287 done

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -41,4 +41,16 @@
 #
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+    	slow = nums[0]
+    	fast = nums[slow]
+    	while fast != slow:
+    		slow = nums[slow]
+    		fast = nums[nums[fast]]
+
+    	slow = 0
+    	while slow != fast:
+    		slow = nums[slow]
+    		fast = nums[fast]
+    	return slow
+
         
