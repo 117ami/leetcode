a7c8c0c0fc391f1fb828a36223477bc55242bdd9
ssrzz <ssrzz@pm.me>
2019-05-11 22:09:51 +0800

    [Easy] p922 python/cpp solutions added

--- test.cpp
+++ test.cpp
@@ -1,12 +1,11 @@
 
 #include "aux.cpp"
-#include "448.find-all-numbers-disappeared-in-an-array.cpp"
+#include "922.sort-array-by-parity-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> nums;// = {4,3,2,7,8,2,3,1};
-	nums = {1, 2, 4, 4, 5};
-	s.findDisappearedNumbers(nums);		
+	vector<int> input{4, 2, 5,7};
+	s.sortArrayByParityII(input);
 	return 0;
 }
 
