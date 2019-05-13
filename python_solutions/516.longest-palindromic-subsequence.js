17d444069aa0a6845d1e0cd74a8d532c90eaaab7
ssrzz <ssrzz@pm.me>
2019-05-13 16:39:11 +0800

    [Medium] p516 js solution added

--- python_solutions/516.longest-palindromic-subsequence.js
+++ python_solutions/516.longest-palindromic-subsequence.js
@@ -45,5 +45,17 @@
  * @return {number}
  */
 var longestPalindromeSubseq = function(s) {
-    
+    var cur = new Array(s.length).fill(0);
+    for (var i = s.length - 1; i >= 0; i--) {
+        var pre = cur.slice();
+        cur[i] = 1;
+        for (var j = i + 1; j < s.length; j++) {
+            cur[j] = s[i] == s[j] ? 2 + pre[j - 1] : Math.max(pre[j], cur[j - 1]);
+        }
+    }
+    return cur[cur.length - 1];
 };
+
+
+s = "abbc"
+longestPalindromeSubseq(s)
