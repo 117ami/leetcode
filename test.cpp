489c39aa536796a4f5beba5e0f803223b7c6d38b
ssrzz <ssrzz@pm.me>
2019-05-13 18:15:46 +0800

    [Easy] p866 cpp/ruby solution added

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
 
 #include "aux.cpp"
-#include "687.longest-univalue-path.cpp"
+#include "860.lemonade-change.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	std::vector<int> v = {5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5};
+	bool r = s.lemonadeChange(v); 
+	say(r);
+	int kkk = 0, b = 4; 
+	kkk += b == 4;
+	say(kkk);
 	return 0;
 }
 
