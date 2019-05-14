448879b952fa36474c00e18e999654de06ec63b7
ssrzz <ssrzz@pm.me>
2019-05-14 15:45:18 +0800

    [Easy] p687 ruby solution added

--- python_solutions/687.longest-univalue-path.rb
+++ python_solutions/687.longest-univalue-path.rb
@@ -13,47 +13,47 @@
 #
 # Given a binary tree, find the length of the longest path where each node in
 # the path has the same value. This path may or may not pass through the root.
-# 
+#
 # The length of path between two nodes is represented by the number of edges
 # between them.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
+#
 # Input:
-# 
-# 
+#
+#
 # ⁠             5
 # ⁠            / \
 # ⁠           4   5
 # ⁠          / \   \
 # ⁠         1   1   5
-# 
-# 
+#
+#
 # Output: 2
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
+#
 # Input:
-# 
-# 
+#
+#
 # ⁠             1
 # ⁠            / \
 # ⁠           4   5
 # ⁠          / \   \
 # ⁠         4   4   5
-# 
-# 
+#
+#
 # Output: 2
-# 
-# 
-# 
+#
+#
+#
 # Note: The given binary tree has not more than 10000 nodes. The height of the
 # tree is not more than 1000.
-# 
+#
 #
 # Definition for a binary tree node.
 # class TreeNode
@@ -67,5 +67,18 @@
 # @param {TreeNode} root
 # @return {Integer}
 def longest_univalue_path(root)
-    
+  gv = 0
+  dfs = lambda do |r|
+    return 0 if r.nil?
+
+    lv = rv = 0
+    lv = dfs.call(r.left) if r.left
+    rv = dfs.call(r.right) if r.right
+    lvp = r.left && r.left.val == r.val ? lv + 1 : 0
+    rvp = r.right && r.right.val == r.val ? rv + 1 : 0
+    gv = [gv, lvp + rvp].max
+    return [lvp, rvp].max
+  end
+  dfs.call(root)
+  gv
 end
