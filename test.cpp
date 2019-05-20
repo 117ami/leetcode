e50d19390c6b94ac68411f3cd81adaa637f66432
ssrzz <ssrzz@pm.me>
2019-05-20 16:43:38 +0800

    [Easy] p824 ruby solution added

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "312.burst-balloons.cpp"
+#include "824.goat-latin.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> nums = {3, 1, 5, 8};
-	say(s.maxCoins(nums));
 	return 0;
 }
 
