ec74114638d0b8e46105fb5cfd327952cedc6857
ssrzz <ssrzz@pm.me>
2019-05-30 10:04:11 +0800

    p494 cpp solution added

--- test.cpp
+++ test.cpp
@@ -1,13 +1,16 @@
 
 #include "aux.cpp"
-#include "c.cpp"
-#include "416.partition-equal-subset-sum.cpp"
+// #include "c.cpp"
+// #include "416.partition-equal-subset-sum.cpp"
+#include "494.target-sum.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
 	VI nums = {1, 5, 11, 9};
 		// nums = {100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100};
-	say(s.canPartition(nums));
+	nums = {1, 1, 1, 1, 1};
+	// say(s.canPartition(nums));
+	s.findTargetSumWays(nums, 3);
 	return 0;
 }
 
