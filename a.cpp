2cfee7695ab9960fef16cd1d5758048315cdbeae
ssrzz <ssrzz@pm.me>
2019-05-30 10:09:08 +0800

    p494 cpp solution added

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include "c.cpp"
+
+int main(int argc, char const *argv[])
+{
+	VI nums {1, 2, 4, 3, 2};
+	int mus = sum_(nums);
+	VI dp(mus + 1, 0);
+	dp[0]=1;
+	for(auto &x: nums)
+		DWN_1(i, mus, x) 
+		dp[i] += dp[i-x];
+
+	EACH(i, dp.size()-1)
+		cout << i << " " << dp[i] << endl;
+	return 0;
+}
