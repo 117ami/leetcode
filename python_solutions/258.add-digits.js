edb328abeac7d947b1abc2e62b2215b0417a444e
ssrzz <ssrzz@pm.me>
2019-06-01 11:38:42 +0800

    [Easy] p258 cpp/js/ruby/python solutions added

---
+++ python_solutions/258.add-digits.js
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=258 lang=javascript
+ *
+ * [258] Add Digits
+ *
+ * https://leetcode.com/problems/add-digits/description/
+ *
+ * algorithms
+ * Easy (54.09%)
+ * Total Accepted:    238.5K
+ * Total Submissions: 440.7K
+ * Testcase Example:  '38'
+ *
+ * Given a non-negative integer num, repeatedly add all its digits until the
+ * result has only one digit.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 38
+ * Output: 2 
+ * Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+ * Since 2 has only one digit, return it.
+ * 
+ * 
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
+ */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var bisect_left = function(nums, target) {
+    var i = 0,
+        j = nums.length - 1,
+        mid;
+    while (i < j) {
+        mid = ~~((i + j) / 2);
+        if (nums[mid] < target)
+            i = mid + 1;
+        else
+            j = mid;
+    }
+    return i;
+}
+
+var bisect_right = function(nums, target) {
+    var i = 0,
+        j = nums.length - 1,
+        mid;
+    while (i < j) {
+        mid = Math.ceil((i + j) / 2);
+        if (nums[mid] > target)
+            j = mid - 1;
+        else
+            i = mid;
+    }
+    return j;
+}
+
+var two_d_array = function(m, n) {
+    return new Array(m).fill(0).map(() => new Array(n).fill(0));
+}
+
+var reverse = function(s) {
+    return [...str].reverse().join('');
+}
+
+
+// simple print
+var print = function(a) {
+    console.log(a);
+}
+
+// For counting elements in a list 
+var counter = function(array_or_string) {
+    var dict = {};
+    for (let i = 0; i < array_or_string.length; i++) {
+        let e = array_or_string[i];
+        dict[e] = dict[e] ? dict[e] += 1 : 1;
+    }
+    return dict;
+}
+
+var sum = function(array) {
+    return array.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+}
+
+var isodd = function(n) {
+    return n % 2 == 1;
+}
+
+var iseven = function(n) {
+    return n % 2 == 0;
+}
+
+var list = function(size, value) {
+    return Array(size).fill(value);
+}
+
+
+var len = function(str_or_array) {
+    return str_or_array.length;
+}
+
+var addDigits = function(num) {
+    return 1 + Math.min(num - 1, (num - 1)  % 9); 
+};
--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 
 #include "aux.cpp"
 #include "c.cpp"
-#include "836.rectangle-overlap.cpp"
+#include "258.add-digits.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
