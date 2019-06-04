0a85bdd9536d28e1ca7647e79ba60103c7c4c9a6
ssrzz <ssrzz@pm.me>
2019-06-04 16:37:02 +0800

    p1071 python/js solutions added

--- python_solutions/1071.greatest-common-divisor-of-strings.js
+++ python_solutions/1071.greatest-common-divisor-of-strings.js
@@ -1,3 +1,61 @@
+/*
+ * @lc app=leetcode id=1071 lang=javascript
+ *
+ * [1071] Greatest Common Divisor of Strings
+ *
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+ *
+ * algorithms
+ * Easy (48.82%)
+ * Total Accepted:    3.1K
+ * Total Submissions: 6.4K
+ * Testcase Example:  '"ABCABC"\n"ABC"'
+ *
+ * For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T
+ * concatenated with itself 1 or more times)
+ * 
+ * Return the largest string X such that X divides str1 and X divides str2.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: str1 = "ABCABC", str2 = "ABC"
+ * Output: "ABC"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: str1 = "ABABAB", str2 = "ABAB"
+ * Output: "AB"
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: str1 = "LEET", str2 = "CODE"
+ * Output: ""
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= str1.length <= 1000
+ * 1 <= str2.length <= 1000
+ * str1[i] and str2[i] are English uppercase letters.
+ * 
+ */
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+
 var bisect_left = function(nums, target) {
     var i = 0,
         j = nums.length - 1,
@@ -39,6 +97,10 @@ var reverse = function(s) {
 var print = function(a) {
     console.log(a);
 }
+
+var say = function(a) {
+    console.log(a)
+};
 
 // For counting elements in a list 
 var counter = function(array_or_string) {
@@ -88,6 +150,19 @@ var gcd = function(a, b) {
     return b == 0 ? a : gcd(b, a % b);
 }
 
-var say = function(a) {
-    console.log(a)
+var divide = function(s, z) {
+    for (let i = 0; i < len(s); i++)
+        if (s[i] != s[i % z]) return false;
+    return true;
+}
+
+var gcdOfStrings = function(s1, s2) {
+    var m = len(s1),
+        n = len(s2),
+        z = gcd(m, n);
+    if (divide(s1, z) && divide(s2, z) && s1.substring(0, z) == s2.substring(0, z))
+        return s1.substring(0, z);
+    return ""
 };
+
+print(gcdOfStrings("ABAB", "ABABAB"))
--- python_solutions/1071.greatest-common-divisor-of-strings.py
+++ python_solutions/1071.greatest-common-divisor-of-strings.py
@@ -13,47 +13,55 @@
 #
 # For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T
 # concatenated with itself 1 or more times)
-# 
+#
 # Return the largest string X such that X divides str1 and X divides str2.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: str1 = "ABCABC", str2 = "ABC"
 # Output: "ABC"
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: str1 = "ABABAB", str2 = "ABAB"
 # Output: "AB"
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: str1 = "LEET", str2 = "CODE"
 # Output: ""
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # Note:
-# 
-# 
+#
+#
 # 1 <= str1.length <= 1000
 # 1 <= str2.length <= 1000
 # str1[i] and str2[i] are English uppercase letters.
-# 
 #
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
+#
+import math
 
+
+class Solution:
+    def divide(self, s, m):
+        return all(s[i] == s[i % m] for i in range(len(s)))
+
+    def gcdOfStrings(self, s1, s2):
+        z = math.gcd(len(s1), len(s2))
+        if self.divide(s1, z) and self.divide(s2, z) and s1[0:z] == s2[0:z]:
+            return s1[0:z]
+        return ""
 
 
 s = Solution()
-
-
+print(s.gcdOfStrings("ABABAB", "ABAB"))
