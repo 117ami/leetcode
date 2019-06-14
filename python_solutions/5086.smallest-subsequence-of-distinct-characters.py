6e7014072a29c9ac1fc2f02d594edf0845eb8f71
ssrzz <ssrzz@pm.me>
2019-06-14 20:31:35 +0800

    p5086 python solution added

--- python_solutions/5086.smallest-subsequence-of-distinct-characters.py
+++ python_solutions/5086.smallest-subsequence-of-distinct-characters.py
@@ -13,61 +13,74 @@
 #
 # Return the lexicographically smallest subsequence of text that contains all
 # the distinct characters of text exactly once.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: "cdadabcc"
 # Output: "adbc"
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: "abcd"
 # Output: "abcd"
-# 
-# 
-# 
+#
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: "ecbacba"
 # Output: "eacb"
-# 
-# 
-# 
+#
+#
+#
 # Example 4:
-# 
-# 
+#
+#
 # Input: "leetcode"
 # Output: "letcod"
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # Note:
-# 
-# 
+#
+#
 # 1 <= text.length <= 1000
 # text consists of lowercase English letters.
-# 
-# 
-# 
-# 
-# 
-# 
-# 
 #
-class Solution:
-    def smallestSubsequence(self, text: str) -> str:
-        
+#
+#
+#
+#
+#
+#
+#
+import collections
 
+
+class Solution:
+    def smallestSubsequence(self, text):
+        cnt = collections.Counter(text)
+        st = []
+        seen = {}
+        for c in text:
+            cnt[c] -= 1
+            if c in seen and seen[c] > 0:
+                continue
+            seen[c] = seen.get(c, 0) + 1
+            while len(st) > 0 and st[-1] > c and cnt[st[-1]] > 0:
+                z = st.pop()
+                seen[z] = 0
+            st.append(c)
+        return ''.join(st)
 
 
 s = Solution()
-
-
+print(s.smallestSubsequence('cdadabcc'))
