8845749a6954e625796e6bb59ad56f4d7fde4e50
ssrzz <ssrzz@pm.me>
2019-06-17 11:40:36 +0800

    [Easy] p1089 ruby /cpp solutions added

--- test.cpp
+++ test.cpp
@@ -1,13 +1,12 @@
 
 #include "aux.cpp"
 #include "c.cpp"
-#include "313.super-ugly-number.cpp"
+#include "1089.duplicate-zeros.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi primes{2,7,13,19};
-	primes = {7,19,29,37,41,47,53,59,61,79,83,89,101,103,109,127,131,137,139,157,167,179,181,199,211,229,233,239,241,251};
-	say(s.nthSuperUglyNumber(100000, primes));
+	vi arr ={1,5,2,0,6,8,0,6,0};
+	s.duplicateZeros(arr);
 	return 0;
 }
 
