19a6fb37995ad222912d615ce389ae2e615d139e
ssrzz <ssrzz@pm.me>
2019-06-25 15:22:34 +0800

    p475 cpp/js solutions added

---
+++ 475.heaters.js
@@ -0,0 +1,368 @@
+var bisect_left = function(nums, target) {
+    var i = 0,
+        j = nums.length - 1,
+        mid;
+    while (i < j) {
+        mid = ~~((i + j) / 2);
+        if (nums[mid] < target)
+            i = mid + 1;
+        else
+            j = mid;
+    }
+    return i;
+}
+
+var bisect_right = function(nums, target) {
+    var i = 0,
+        j = nums.length - 1,
+        mid;
+    while (i < j) {
+        mid = Math.ceil((i + j) / 2);
+        if (nums[mid] > target)
+            j = mid - 1;
+        else
+            i = mid;
+    }
+    return j;
+}
+
+var two_d_array = function(m, n) {
+    return new Array(m).fill(0).map(() => new Array(n).fill(0));
+}
+
+var reverse = function(s) {
+    return [...str].reverse().join('');
+}
+
+
+// simple print
+var print = function(a) {
+    console.log(a);
+}
+
+// For counting elements in a list 
+var counter = function(array_or_string) {
+    var dict = {};
+    for (let i = 0; i < array_or_string.length; i++) {
+        let e = array_or_string[i];
+        dict[e] = dict[e] ? dict[e] += 1 : 1;
+    }
+    return dict;
+}
+
+var sum = function(array) {
+    return array.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+}
+
+var isodd = function(n) {
+    return n % 2 == 1;
+}
+
+var iseven = function(n) {
+    return n % 2 == 0;
+}
+
+var list = function(size, value) {
+    return Array(size).fill(value);
+}
+
+
+var len = function(str_or_array) {
+    return str_or_array.length;
+}
+
+var sorted = function(arr) {
+    return arr.sort((a, b) => (a - b));
+}
+
+var sort_by_last = function(arr){
+    return arr.sort((a, b)=>(last(a) - last(b)));
+}
+
+var sort_by_first = function(arr){
+    return arr.sort((a, b)=>(a[0] - b[0]));
+}
+
+// print out a Map 
+function printMap(m) {
+    m.forEach(function(v, k) {
+        console.log(k + " " + v);
+    })
+}
+
+var gcd = function(a, b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+var say = function(a) {
+    console.log(a)
+};
+
+
+var two_d_array = function(m, n, v = 0) {
+    return [...Array(m)].map(x => Array(n).fill(v));
+}
+
+function floor(float_number) {
+    return Math.floor(float_number)
+}
+
+function ceil(float_number) {
+    return Math.ceil(float_number)
+}
+
+
+var max = function(arr) {
+    return Math.max(...arr);
+}
+
+var min = function(arr) {
+    return Math.min(...arr)
+};
+
+var pairmax = function(a, b) {
+    return Math.max(a, b)
+}
+
+var pairmin = function(a, b) {
+    return Math.min(a, b)
+}
+
+// create an array with elements from 0 to n
+var nlist = function(n) {
+    return [...Array(n + 1).keys()];
+}
+
+var ispalindrome = function(s) {
+    var i = 0,
+        j = len(s) - 1;
+    while (i < j) {
+        if (s[i] != s[j]) return false;
+        i += 1;
+        j -= 1;
+    }
+    return true;
+}
+
+function permutations(inputArr) {
+    var results = [];
+
+    function permute(arr, memo) {
+        var cur, memo = memo || [];
+
+        for (var i = 0; i < arr.length; i++) {
+            cur = arr.splice(i, 1);
+            if (arr.length === 0) {
+                results.push(memo.concat(cur));
+            }
+            permute(arr.slice(), memo.concat(cur));
+            arr.splice(i, 0, cur[0]);
+        }
+
+        return results;
+    }
+
+    return permute(inputArr);
+}
+
+var directions = [
+    [-1, -1],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+]
+
+var last = function(arr) {
+    return arr[len(arr) - 1];
+}
+
+var first = function(arr) {
+    return arr[0];
+}
+
+var second = function(arr) {
+    return arr[1];
+}
+
+var third = function(arr) {
+    return arr[2];
+}
+
+function exist(key, hash) {
+    return (key in hash);
+}
+
+// shortest common super-sequence
+var scs = function(s, t) {
+    var m = len(s),
+        n = len(t),
+        dp = two_d_array(m + 1, n + 1);
+    for (let i = 1; i <= m; i++)
+        for (let j = 1; j <= n; j++)
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = pairmax(dp[i - 1][j], dp[i][j - 1]);
+
+    var i = m,
+        j = n,
+        index = m + n - last(last(dp)),
+        res = list(index, '*');
+
+    while (i > 0 && j > 0) {
+        if (s[i - 1] == t[j - 1]) {
+            res[index - 1] = s[i - 1];
+            i -= 1;
+            j -= 1;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            res[index - 1] = s[i - 1];
+            i -= 1;
+        } else {
+            res[index - 1] = t[j - 1];
+            j -= 1;
+        }
+        index -= 1;
+    }
+
+
+    if (i + j == 0) return res.join('');
+    if (j > 0) {
+        i = j;
+        s = t;
+    }
+
+    while (i > 0) {
+        res[index - 1] = s[i - 1];
+        index -= 1;
+        i -= 1;
+    }
+    return res.join('');
+}
+
+// longest common subsequence
+var lcs = function(s, t) {
+    var m = len(s),
+        n = len(t),
+        dp = two_d_array(m + 1, n + 1);
+    for (let i = 1; i <= m; i++)
+        for (let j = 1; j <= n; j++)
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = pairmax(dp[i - 1][j], dp[i][j - 1]);
+
+    var i = m,
+        j = n,
+        index = last(last(dp)),
+        res = list(index, '*');
+
+    while (i > 0 && j > 0) {
+        if (s[i - 1] == t[j - 1]) {
+            res[index - 1] = s[i - 1];
+            index -= 1;
+            i -= 1;
+            j -= 1;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+    return res.join('');
+}
+
+
+var zip = function(lista, listb) {
+    return a.map(function(e, i){return [e, b[i]];});
+}
+
+class Counter {
+    constructor() {
+        this.m = {};
+    }
+
+    read(key) {
+        return exist(key, this.m) ? this.m[key] : 0;
+    }
+
+    plusone(key) {
+        if (!this.m[key]) this.m[key] = 0;
+        this.m[key] += 1;
+    }
+}
+
+/*
+ * @lc app=leetcode id=475 lang=javascript
+ *
+ * [475] Heaters
+ *
+ * https://leetcode.com/problems/heaters/description/
+ *
+ * algorithms
+ * Easy (31.89%)
+ * Total Accepted:    48.8K
+ * Total Submissions: 152.9K
+ * Testcase Example:  '[1,2,3]\n[2]'
+ *
+ * Winter is coming! Your first job during the contest is to design a standard
+ * heater with fixed warm radius to warm all the houses.
+ * 
+ * Now, you are given positions of houses and heaters on a horizontal line,
+ * find out minimum radius of heaters so that all houses could be covered by
+ * those heaters.
+ * 
+ * So, your input will be the positions of houses and heaters seperately, and
+ * your expected output will be the minimum radius standard of heaters.
+ * 
+ * Note:
+ * 
+ * 
+ * Numbers of houses and heaters you are given are non-negative and will not
+ * exceed 25000.
+ * Positions of houses and heaters you are given are non-negative and will not
+ * exceed 10^9.
+ * As long as a house is in the heaters' warm radius range, it can be
+ * warmed.
+ * All the heaters follow your radius standard and the warm radius will the
+ * same.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3],[2]
+ * Output: 1
+ * Explanation: The only heater was placed in the position 2, and if we use the
+ * radius 1 standard, then all the houses can be warmed.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4],[1,4]
+ * Output: 1
+ * Explanation: The two heater was placed in the position 1 and 4. We need to
+ * use radius 1 standard, then all the houses can be warmed.
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} houses
+ * @param {number[]} heaters
+ * @return {number}
+ */
+var findRadius = function(houses, heaters) {
+    
+};
--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
 
 #include "aux.cpp"
 #include "c.cpp"
-#include "852.peak-index-in-a-mountain-array.cpp"
+#include "475.heaters.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vi houses = {1,2,3}, heaters={2};
+	say(s.findRadius(houses, heaters));
 	return 0;
 }
 
