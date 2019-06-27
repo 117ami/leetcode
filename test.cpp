a97b4d9b52068e454d887a8c122e4e9be8a851c4
ssrzz <ssrzz@pm.me>
2019-06-27 13:02:57 +0800

    p378 py/cpp solutions added

--- test.cpp
+++ test.cpp
@@ -1,13 +1,13 @@
 
 #include "aux.cpp"
 #include "c.cpp"
-#include "744.find-smallest-letter-greater-than-target.cpp"
+#include "378.kth-smallest-element-in-a-sorted-matrix.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vc letters{'c', 'f', 'k'}; 
-	char target = 'k';
-	say(s.nextGreatestLetter(letters, target));
+	vvi matrix = {{1,5,9},{10,11,13},{12,13,15}}; 
+	int k = 7; 
+	say(s.kthSmallest(matrix, k));
 	return 0;
 }
 
