45ec23462d690811db2dddf09660d93b85bed46d
ssrzz <ssrzz@pm.me>
2019-07-03 00:25:29 +0800

    [Hard] p1106 python solution added

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 
 #include "aux.cpp"
-#include "1104.path-in-zigzag-labelled-binary-tree.cpp"
+#include "1106.parsing-a-boolean-expression.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.pathInZigZagTree(14));
 	return 0;
 }
 
