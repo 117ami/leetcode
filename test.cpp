f054da5ae7d11d3581bcdcfddd4ced7c4080225d
ssrzz <ssrzz@pm.me>
2019-07-09 14:45:17 +0800

    [Easy] p1108 py/js/cpp solutions added

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 
 #include "aux.cpp"
-#include "430.flatten-a-multilevel-doubly-linked-list.cpp"
+#include "1108.defanging-an-ip-address.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string a = "1.2.3.42";
+	say(s.defangIPaddr(a));
+
 	return 0;
 }
 
