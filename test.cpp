cad5e49b39702605441e76c93874913e6aca78eb
ssrzz <ssrzz@pm.me>
2019-07-23 18:44:04 +0800

    [Medium] p670 py solution added

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "1131.maximum-of-absolute-value-expression.cpp"
+#include "670.maximum-swap.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi a = {1,-2,-5,0,10}, b = {0,-2,-1,-7,-4};
-	say(s.maxAbsValExpr(a, b));
-
 	return 0;
 }
 
