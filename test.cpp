802d2182bd288ddefdbb3c9291fa81077ce07c67
ssrzz <ssrzz@pm.me>
2019-07-29 22:15:34 +0800

    [Easy] p680 cpp/js/python solutions added

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 
 #include "aux.cpp"
-#include "437.path-sum-iii.cpp"
+#include "680.valid-palindrome-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string st = "deeeeeeee";
+	// st = "abcdefedbcba";
+	st = "abcbdda";
+
+	say(s.validPalindrome(st));
 	return 0;
 }
 
