ed6c23aba3bc1459edb813fe8bbabd45f0d0b0b5
ssrzz <ssrzz@pm.me>
2019-08-01 22:16:48 +0800

    p164 maximum gap cpp/python solutions added

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 
 #include "aux.cpp"
-#include "385.mini-parser.cpp"
+#include "164.maximum-gap.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vi nums = {3, 6, 9 , 1, 19}; 
+	nums = {1, 1000};
+	say(s.maximumGap(nums));
 	return 0;
 }
 
