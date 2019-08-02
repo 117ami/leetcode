2d882ed1eff64e790203de16e31d08536360424b
ssrzz <ssrzz@pm.me>
2019-08-02 13:40:12 +0800

    p509 simple fibonacchi cpp/js/py solutions added

--- test.cpp
+++ test.cpp
@@ -1,12 +1,10 @@
 
 #include "aux.cpp"
-#include "164.maximum-gap.cpp"
+#include "509.fibonacci-number.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi nums = {3, 6, 9 , 1, 19}; 
-	nums = {1, 1000};
-	say(s.maximumGap(nums));
+	say(s.fib(30));
 	return 0;
 }
 
