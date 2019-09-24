f6a595ce175f4dbb33eeeddd998977f03b45c42c
ssrzz <ssrzz@pm.me>
2019-09-24 22:17:56 +0800

    [easy] 404 sum of left leaves solved

--- test.cpp
+++ test.cpp
@@ -1,13 +1,9 @@
 
 #include "aux.cpp"
-#include "1200.minimum-absolute-difference.cpp"
+#include "404.sum-of-left-leaves.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	// vi arr = {3,8,-10,23,19,-4,-14,27}; 
-	vi arr = {4,2,1,3};
-	vvi res = s.minimumAbsDifference(arr); 
-	say(res);
 	return 0;
 }
 
