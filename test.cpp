e14f6c701275550bf6aa912c521c070b27744378
ssrzz <ssrzz@pm.me>
2019-09-27 15:46:37 +0800

    457 circular array loop cpp solution added

--- test.cpp
+++ test.cpp
@@ -4,6 +4,13 @@
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vi nums = {2, -1, 1, 2, 2};
+	// nums = {-2, 1, -1, -2, -2};
+	// nums = {-1, -2, -3, -4, -5};
+	// nums = {-1,2,1,2};
+	vi copy(nums);
+	say(s.circularArrayLoop(nums));
+	say(s.circularArrayLoop_2(copy));
 	return 0;
 }
 
