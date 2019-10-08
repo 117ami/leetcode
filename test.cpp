abab7a0d035112ca689e61e598455a94e333ef49
NilLau <ssruoz@gmail.com>
2019-10-08 21:24:54 +0800

    [Medium] p18 4sum python solution added

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1207.unique-number-of-occurrences.cpp"
+#include "18.4sum.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> arr = {1,2,3,1,2}; 
-	say(s.uniqueOccurrences(arr));
 	return 0;
 }
 
