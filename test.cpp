4885269516df651b0f875b3e37045871e4883e62
NilLau <ssruoz@gmail.com>
2019-10-27 14:31:14 +0800

    p1237-E find positive integer solution python/cpp added

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "1232.check-if-it-is-a-straight-line.cpp"
+#include "1237.find-positive-integer-solution-for-a-given-equation.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<vector<int>> cs = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
-	bool res = s.checkStraightLine(cs);
-	say(res);
 	return 0;
 }
 
