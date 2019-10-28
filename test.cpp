f19d0b14fcb5df0a73d4dc8cffc844c7566714d5
NilLau <ssruoz@gmail.com>
2019-10-28 11:03:39 +0800

    p970-E powerful integers solved with cpp/js/py

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "295.find-median-from-data-stream.cpp"
+#include "970.powerful-integers.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	int x = 2, y = 3, bound = 10; 
+	say(s.powerfulIntegers(x, y, bound));
 	return 0;
 }
 
