abdb2a44c15935fef27bffae917ae453842fd182
NilLau <ssruoz@gmail.com>
2019-11-01 16:59:28 +0800

    p1095-H find in moutain array resolved with python

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "852.peak-index-in-a-mountain-array.cpp"
+#include "1095.find-in-mountain-array.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> arr = {0,2,1,0}; 
-	say(s.peakIndexInMountainArray(arr));
 	return 0;
 }
 
