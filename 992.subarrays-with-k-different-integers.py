6b086bbcaed90dc6e8badcd2c4553635fdf92680
NilLau <ssruoz@gmail.com>
2019-11-07 16:01:40 +0800

    p922-H subarrays with K different arrays solved with Py/CPP

---
+++ 992.subarrays-with-k-different-integers.py
@@ -0,0 +1,101 @@
+#
+# @lc app=leetcode id=992 lang=python3
+#
+# [992] Subarrays with K Different Integers
+#
+# https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+#
+# algorithms
+# Hard (45.89%)
+# Total Accepted:    13.5K
+# Total Submissions: 29.3K
+# Testcase Example:  '[1,2,1,2,3]\n2'
+#
+# Given an array A of positive integers, call a (contiguous, not necessarily
+# distinct) subarray of A good if the number of different integers in that
+# subarray is exactly K.
+#
+# (For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
+#
+# Return the number of good subarrays of A.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: A = [1,2,1,2,3], K = 2
+# Output: 7
+# Explanation: Subarrays formed with exactly 2 different integers: [1,2],
+# [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2].
+#
+#
+# Example 2:
+#
+#
+# Input: A = [1,2,1,3,4], K = 3
+# Output: 3
+# Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3],
+# [2,1,3], [1,3,4].
+#
+#
+#
+#
+# Note:
+#
+#
+# 1 <= A.length <= 20000
+# 1 <= A[i] <= A.length
+# 1 <= K <= A.length
+#
+#
+import collections
+
+
+class Solution:
+    def subarraysWithKDistinct2(self, A, K):
+        i = j = 0
+        c = collections.Counter()
+        res = 0
+        while j < len(A):
+            print(i, j)
+            while len(c) < K and j < len(A):
+                c[A[j]] += 1
+                j += 1
+            
+            if len(c) < K or j >= len(A): return res 
+            j -= 1
+            
+            while i <= j and len(c) == K:
+                c[A[i]] -= 1
+                if c[A[i]] == 0:
+                    del c[A[i]]
+                res += j - i
+                i += 1
+        return res 
+
+
+    def subarraysWithKDistinct(self, A, K):
+        return self.atmostk(A, K) - self.atmostk(A, K - 1)
+
+    def atmostk(self, A, K):
+        c = collections.Counter()
+        res = i = 0
+        for j in range(len(A)):
+            if c[A[j]] == 0:
+                K -= 1
+            c[A[j]] += 1
+
+            while K < 0:
+                c[A[i]] -= 1
+                if c[A[i]] == 0:
+                    K += 1
+                i += 1
+            res += j - i + 1
+        return res
+
+
+s = Solution()
+A = [1, 2, 1, 2, 3]
+K = 2
+print(s.subarraysWithKDistinct2(A, K))
--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1249.minimum-remove-to-make-valid-parentheses.cpp"
+#include "992.subarrays-with-k-different-integers.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	string ss = "a(b(c(d)e)f";
-	say(s.minRemoveToMakeValid(ss));
 	return 0;
 }
 
