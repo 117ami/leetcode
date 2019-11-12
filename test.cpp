cf6c6e4568cf2be91745beb3366ec869656638ad
NilLau <ssruoz@gmail.com>
2019-11-12 16:20:34 +0800

    p1252-E cells with odd values in a matrix solved with CPP/js/Py

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 
 #include "aux.cpp"
-#include "977.squares-of-a-sorted-array.cpp"
+#include "1252.cells-with-odd-values-in-a-matrix.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	int n=2, m=3; 
+	vector<vector<int>> ids {{0, 1}, {1,1}};
+	say(s.oddCells(n,m,ids));
 	return 0;
 }
 
