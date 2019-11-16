47f837ea57f5193a64da44e9ca9a74e8814c46c1
NilLau <ssruoz@gmail.com>
2019-11-16 22:21:53 +0800

    p1238-M circular permutation in binary representation [py/cpp]

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 
 #include "aux.cpp"
-#include "1219.path-with-maximum-gold.cpp"
+#include "1238.circular-permutation-in-binary-representation.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	
 	return 0;
 }
 
