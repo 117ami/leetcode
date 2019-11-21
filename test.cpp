bcb1729d06ffc708f746b2ee4eda17add8f6893a
NilLau <ssruoz@gmail.com>
2019-11-21 23:01:40 +0800

    p1261-M find elements in a contaimnated B-tree [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,13 +1,9 @@
 
 #include "aux.cpp"
-#include "1260.shift-2d-grid.cpp"
+#include "1261.find-elements-in-a-contaminated-binary-tree.cpp"
 
 int main(int argc, char const *argv[]) {
-	Solution s;
-	vector<vector<int>> grid = {{1,2,3},{4,5,6}, {7,8,9}};
-	int k = 1; 
-	vector<vector<int>> res = s.shiftGrid(grid, k);
-	say(res);
+	// Solution s;
 	return 0;
 }
 
