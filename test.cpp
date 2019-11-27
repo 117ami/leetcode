66a3969429deb2b6dbfbe63771e8308034b7f3a8
NilLau <ssruoz@gmail.com>
2019-11-27 19:20:45 +0800

    p1250-H check if a good array [CPP/Python3]

--- test.cpp
+++ test.cpp
@@ -1,13 +1,9 @@
 
 #include "aux.cpp"
-#include "1268.search-suggestions-system.cpp"
+#include "1250.check-if-it-is-a-good-array.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<string> products = {"mobile","mouse","moneypot","monitor","mousepad"};
-	string sw = "mouse"; 
-	vector<vector<string>> res = s.suggestedProducts(products, sw); 
-	say(res);
 	return 0;
 }
 
