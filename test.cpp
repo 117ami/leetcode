5a7997efb14fbca068bb780e5a688a2078a9ffdb
117ami <ssruoz@gmail.com>
2019-12-13 07:06:20 +0800

    p892-Easy leaf similar trees [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "893.groups-of-special-equivalent-strings.cpp"
+#include "872.leaf-similar-trees.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vs a = {"abc","acb","bac","bca","cab","cba"};
-	say(s.numSpecialEquivGroups(a));
 	return 0;
 }
 
