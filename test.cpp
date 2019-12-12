37afc930485ff18fdf3f537f94f47b6c8fa60af2
NilLau <ssruoz@gmail.com>
2019-12-12 17:33:42 +0800

    p921-Medium minimum add to make parentheses valid [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "980.unique-paths-iii.cpp"
+#include "921.minimum-add-to-make-parentheses-valid.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vvi grid = {{1,0,0,0},{0,0,0,0},{0,0,0,2}};
-	say(s.uniquePathsIII(grid));
 	return 0;
 }
 
