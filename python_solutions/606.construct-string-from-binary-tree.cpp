94b82e19c1ec59786ea40086b624cf28d69093cc
117ami <ssruoz@gmail.com>
2019-12-15 09:19:23 +0800

    p606-Easy construct string from binary tree [CPP/Python]

--- python_solutions/606.construct-string-from-binary-tree.cpp
+++ python_solutions/606.construct-string-from-binary-tree.cpp
@@ -585,3 +585,82 @@ vector<int> char_counter(string chars) {
     cc[c - 'a'] += 1;
   return cc;
 }
+/*
+ * @lc app=leetcode id=606 lang=cpp
+ *
+ * [606] Construct String from Binary Tree
+ *
+ * https://leetcode.com/problems/construct-string-from-binary-tree/description/
+ *
+ * algorithms
+ * Easy (52.62%)
+ * Total Accepted:    69.5K
+ * Total Submissions: 132.2K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * You need to construct a string consists of parenthesis and integers from a
+ * binary tree with the preorder traversing way.
+ * 
+ * The null node needs to be represented by empty parenthesis pair "()". And
+ * you need to omit all the empty parenthesis pairs that don't affect the
+ * one-to-one mapping relationship between the string and the original binary
+ * tree.
+ * 
+ * Example 1:
+ * 
+ * Input: Binary tree: [1,2,3,4]
+ * ⁠      1
+ * ⁠    /   \
+ * ⁠   2     3
+ * ⁠  /    
+ * ⁠ 4     
+ * 
+ * Output: "1(2(4))(3)"
+ * Explanation: Originallay it needs to be "1(2(4)())(3()())", but you need to
+ * omit all the unnecessary empty parenthesis pairs. And it will be
+ * "1(2(4))(3)".
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: Binary tree: [1,2,3,null,4]
+ * ⁠      1
+ * ⁠    /   \
+ * ⁠   2     3
+ * ⁠    \  
+ * ⁠     4 
+ * 
+ * Output: "1(2()(4))(3)"
+ * Explanation: Almost the same as the first example, except we can't omit the
+ * first parenthesis pair to break the one-to-one mapping relationship between
+ * the input and the output.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if (!t) return "";
+        string res = itos(t->val); 
+        if (t->left) {
+          res += "(" + tree2str(t->left) + ")";
+          if (t->right) res += "(" + tree2str(t->right) + ")";
+        } else if (t->right) 
+          res += "()(" + tree2str(t->right) + ")";
+        return res; 
+    }
+};
+
+
+
+static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL);return 0; }();
--- test.cpp
+++ test.cpp
@@ -1,18 +1,9 @@
 
 #include "aux.cpp"
-#include "655.print-binary-tree.cpp"
+#include "606.construct-string-from-binary-tree.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi vs = {1, 2};
-	vs = {1, 2, 3, INT_MIN, 4};
-	TreeNode* root = growTreeFromList(vs);
-	vvs res = s.printTree(root); 
-	say(res[0].size());
-	say(res);
-
-	float i = 9.1, pi = pow(9, 2); 
-	cout << typeid(pi).name() << endl; 
 	return 0;
 }
 
