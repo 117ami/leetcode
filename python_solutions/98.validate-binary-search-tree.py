1116e8b15bc6d029f788d76411621943d03fac52
117ami <ssruoz@gmail.com>
2019-12-16 16:32:40 +0800

    p98-Medium validate BST [Python/CPP]

--- python_solutions/98.validate-binary-search-tree.py
+++ python_solutions/98.validate-binary-search-tree.py
@@ -1,9 +1,3 @@
-
-from aux import * 
-import collections
-import functools
-import bisect
-
 #
 # @lc app=leetcode id=98 lang=python3
 #
@@ -18,44 +12,44 @@ import bisect
 # Testcase Example:  '[2,1,3]'
 #
 # Given a binary tree, determine if it is a valid binary search tree (BST).
-# 
+#
 # Assume a BST is defined as follows:
-# 
-# 
+#
+#
 # The left subtree of a node contains only nodes with keys less than the node's
 # key.
 # The right subtree of a node contains only nodes with keys greater than the
 # node's key.
 # Both the left and right subtrees must also be binary search trees.
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # ⁠   2
 # ⁠  / \
 # ⁠ 1   3
-# 
+#
 # Input: [2,1,3]
 # Output: true
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # ⁠   5
 # ⁠  / \
 # ⁠ 1   4
 # / \
 # 3   6
-# 
+#
 # Input: [5,1,4,null,null,3,6]
 # Output: false
 # Explanation: The root node's value is 5 but its right child's value is 4.
-# 
-# 
+#
+#
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -64,26 +58,26 @@ import bisect
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        self.prev = -1 << 32 - 1
-        self.quit = False 
+        self.prev = -1 << 33
+        self.quit = False
 
         def preorder(node):
-            if not node or self.quit: return 
+            if not node or self.quit:
+                return
             preorder(node.left)
             if self.prev >= node.val:
                 self.quit = True
             else:
                 self.prev = node.val
                 preorder(node.right)
-        
+
         preorder(root)
         return not self.quit
 
-from aux import *
-s = Solution()
-root = Trees().listToTree([1,1])
-print(s.isValidBST(root))
-
-
+# from aux import *
+# s = Solution()
+# root = Trees().listToTree([1,1])
+# print(s.isValidBST(root))
--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 
 #include "aux.cpp"
-#include "99.recover-binary-search-tree.cpp"
+#include "98.validate-binary-search-tree.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	s.isValidBST(nullptr);
 	return 0;
 }
 
