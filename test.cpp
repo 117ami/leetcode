a56b657c035349ce1084ecc1c96078ac82eca1aa
117ami <ssruoz@gmail.com>
2019-12-17 14:54:15 +0800

    p791-Medium custom sort string [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,10 @@
 
 #include "aux.cpp"
-#include "797.all-paths-from-source-to-target.cpp"
+#include "791.custom-sort-string.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vvi g = {{1,2},{3},{3},{}};
-	vvi res = s.allPathsSourceTarget(g);
-	say(res);
+	say(s.customSortString("abc", "abcdabaaccabaacaadaabaaceabaccae"));
 	return 0;
 }
 
