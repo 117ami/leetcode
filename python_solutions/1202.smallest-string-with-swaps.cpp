bcd41c8885542e2d2eec7b42a3b724acc7d70354
117ami <ssruoz@gmail.com>
2019-12-19 07:47:54 +0800

    p1202-Medium smallest string with swaps [CPP/Python]

--- python_solutions/1202.smallest-string-with-swaps.cpp
+++ python_solutions/1202.smallest-string-with-swaps.cpp
@@ -601,14 +601,109 @@ vector<int> addTwoVector(vector<int> &a, vector<int>& b) {
 
 
 int last(vector<int> &v) { return v[v.size()-1]; }
-
+/*
+ * @lc app=leetcode id=1202 lang=cpp
+ *
+ * [1202] Smallest String With Swaps
+ *
+ * https://leetcode.com/problems/smallest-string-with-swaps/description/
+ *
+ * algorithms
+ * Medium (41.96%)
+ * Total Accepted:    7K
+ * Total Submissions: 16.8K
+ * Testcase Example:  '"dcab"\n[[0,3],[1,2]]'
+ *
+ * You are given a string s, and an array of pairs of indices in the string
+ * pairs where pairs[i] = [a, b] indicates 2 indices(0-indexed) of the string.
+ * 
+ * You can swap the characters at any pair of indices in the given pairs any
+ * number of times.
+ * 
+ * Return the lexicographically smallest string that s can be changed to after
+ * using the swaps.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "dcab", pairs = [[0,3],[1,2]]
+ * Output: "bacd"
+ * Explaination: 
+ * Swap s[0] and s[3], s = "bcad"
+ * Swap s[1] and s[2], s = "bacd"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "dcab", pairs = [[0,3],[1,2],[0,2]]
+ * Output: "abcd"
+ * Explaination: 
+ * Swap s[0] and s[3], s = "bcad"
+ * Swap s[0] and s[2], s = "acbd"
+ * Swap s[1] and s[2], s = "abcd"
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "cba", pairs = [[0,1],[1,2]]
+ * Output: "abc"
+ * Explaination: 
+ * Swap s[0] and s[1], s = "bca"
+ * Swap s[1] and s[2], s = "bac"
+ * Swap s[0] and s[1], s = "abc"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^5
+ * 0 <= pairs.length <= 10^5
+ * 0 <= pairs[i][0], pairs[i][1] < s.length
+ * s only contains lower case English letters.
+ * 
+ * 
+ */
 
 int find(int x, vector<int>& p) {
   if (x != p[x]) p[x] = find(p[x], p);
   return p[x]; 
 }
 
-// Do not use union, since it's a keyword of CPP
 void merge(int x, int y, vector<int>& p){
   p[find(x, p)] = find(y, p);
 }
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+      int n = s.size(); 
+      vi p; fori(i, n) p.pb(i);
+
+      for(auto &pi: pairs) merge(pi[0], pi[1], p); 
+
+      vvi idx(n, vi{});
+      vector<vector<char>> chars (n, vc{});
+      fori(i, n){
+        int fa = find(i, p); 
+        idx[fa].pb(i), chars[fa].pb(s[i]);
+      }
+
+      vc res(n, ' ');
+      fori(i, n){
+        if (idx[i].size() > 0) {
+          vc tmp(chars[i]); 
+          dsort(tmp); 
+          fori(j, idx[i].size()) res[idx[i][j]] = tmp[j]; 
+        }
+      }
+      
+      return string (res.begin(), res.end());
+    }
+};
+
+
+
+static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL);return 0; }();
--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 
 #include "aux.cpp"
-#include "443.string-compression.cpp"
+#include "1202.smallest-string-with-swaps.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string st = "dcab";
+	vvi pairs = {{0, 3}, {1, 2}, {0, 2}} ;
+	st = "dcab", pairs = {{0,3}, {1, 2}};
+	say(s.smallestStringWithSwaps(st, pairs));
 	return 0;
 }
 
