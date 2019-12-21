1a52dd9ce607979281b2c91c623f7264a917c731
117ami <ssruoz@gmail.com>
2019-12-22 07:43:27 +0800

    p912-Easy sort array [CPP/Python]

---
+++ python_solutions/912.sort-an-array.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=912 lang=python3
+#
+# [912] Sort an Array
+#
+# https://leetcode.com/problems/sort-an-array/description/
+#
+# algorithms
+# Medium (62.73%)
+# Total Accepted:    39.8K
+# Total Submissions: 63.4K
+# Testcase Example:  '[5,2,3,1]'
+#
+# Given an array of integers nums, sort the array in ascending order.
+# 
+# 
+# Example 1:
+# Input: nums = [5,2,3,1]
+# Output: [1,2,3,5]
+# Example 2:
+# Input: nums = [5,1,1,2,0,0]
+# Output: [0,0,1,1,2,5]
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 50000
+# -50000 <= nums[i] <= 50000
+# 
+# 
+#
+class Solution:
+    def sortArray(self, ns):
+        return sorted(ns)
+        
+
+
+
+s = Solution()
+
+
--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "969.pancake-sorting.cpp"
+#include "912.sort-an-array.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi a = {1,4,3,2};
-	say(s.pancakeSort(a));
 	return 0;
 }
 
