d101c8e8e77a9649b86957abe61c2ea7ce8dbacd
117ami <ssruoz@gmail.com>
2019-12-23 21:34:14 +0800

    p503-Medium next greater element 2 [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,11 @@
 
 #include "aux.cpp"
-#include "241.different-ways-to-add-parentheses.cpp"
+#include "503.next-greater-element-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	string x = "2*3-4*5"; 
-	// x = "3-4";
-	say(s.diffWaysToCompute(x));
+	vi x = {1,2,5,3,1,2,6,5,4};
+	say(s.nextGreaterElements(x));
 	return 0;
 }
 
