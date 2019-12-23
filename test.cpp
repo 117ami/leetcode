9c4cc6b994d69bd81169d538b801af6a5e6c518e
117ami <ssruoz@gmail.com>
2019-12-23 16:10:13 +0800

    p547-Medium friend circles [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 
 #include "aux.cpp"
-#include "508.most-frequent-subtree-sum.cpp"
+#include "547.friend-circles.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string ss = "[[1,1,0], [1,1,0], [0,0,1]]";
+	ss ="[[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]";
+	vvi m = extractMatrixFromString(ss);
+	say(m);
+	say(s.findCircleNum(m));
 	return 0;
 }
 
