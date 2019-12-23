8e548227047673b10ee893ec875da1785678e29d
117ami <ssruoz@gmail.com>
2019-12-23 08:42:33 +0800

    p888-Easy fair candy swap [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "957.prison-cells-after-n-days.cpp"
+#include "888.fair-candy-swap.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vi a = {1, 3}, b = {2}; 
+	say(s.fairCandySwap(a, b));
 	return 0;
 }
 
