53551f37a870fa29a579a5ec39f6706b3d2411e8
117ami <ssruoz@gmail.com>
2019-12-25 16:07:39 +0800

    p688-Mediu knight probability in chessboard [Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "632.smallest-range-covering-elements-from-k-lists.cpp"
+#include "688.knight-probability-in-chessboard.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vvi nums = extractMatrixFromString("[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]");
-	say(s.smallestRange(nums));
 	return 0;
 }
 
