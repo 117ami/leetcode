e7d3cb7f540839d2ff454675bb2470c7fbae98bd
117ami <ssruoz@gmail.com>
2019-12-26 20:37:41 +0800

    p889-Medium construct BT from preorder/postorder [CPP/Python]

---
+++ python_solutions/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,71 @@
+from collections import Counter, defaultdict, OrderedDict
+from bisect import bisect_left, bisect_right 
+from functools import reduce 
+true = True
+false = False
+#
+# @lc app=leetcode id=889 lang=python3
+#
+# [889] Construct Binary Tree from Preorder and Postorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/description/
+#
+# algorithms
+# Medium (63.42%)
+# Total Accepted:    25.5K
+# Total Submissions: 40.1K
+# Testcase Example:  '[1,2,4,5,3,6,7]\n[4,5,2,6,7,3,1]'
+#
+# Return any binary tree that matches the given preorder and postorder
+# traversals.
+# 
+# Values in the traversals pre and post are distinct positive integers.
+# 
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+# Output: [1,2,3,4,5,6,7]
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# 1 <= pre.length == post.length <= 30
+# pre[] and post[] are both permutations of 1, 2, ..., pre.length.
+# It is guaranteed an answer exists. If there exists multiple answers, you can
+# return any of them.
+# 
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def constructFromPrePost(self, pre, post):
+        if not pre: return 
+        root = TreeNode(pre[0])
+        if len(pre) == 1:
+            return root 
+        idx = post.index(pre[1])
+        post.pop()
+        root.left = self.constructFromPrePost(pre[1:idx+2], post[:idx+1])
+        root.right = self.constructFromPrePost(pre[idx+2:], post[idx+1:])
+        return root 
+        
+pre,post = [1,2,4,5,3,6,7], [4,5,2,6,7,3,1]
+print(post.index(2))
+s = Solution()
+
+
+
--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 
 #include "aux.cpp"
-#include "810.chalkboard-xor-game.cpp"
+#include "889.construct-binary-tree-from-preorder-and-postorder-traversal.cpp"
 
 int main(int argc, char const *argv[]) {
-	Solution s;
-	vi vec {0, 1};
-	say(s.xorGame(vec));
+	// Solution s;
+	vi a = {0, 1,2,3,4,5};
+	vi part(a.begin()+ 1, a.end()-1);
+	say(part);
 	return 0;
 }
 
