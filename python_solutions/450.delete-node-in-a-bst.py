5a9798cebac6a286c1b10bb948725409aed53ed4
117ami <ssruoz@gmail.com>
2019-12-26 15:24:23 +0800

    p450-Medium delete node in a BST [CPP/Python]

---
+++ python_solutions/450.delete-node-in-a-bst.py
@@ -0,0 +1,92 @@
+from collections import Counter, defaultdict, OrderedDict
+from bisect import bisect_left, bisect_right 
+true = True
+false = False
+#
+# @lc app=leetcode id=450 lang=python3
+#
+# [450] Delete Node in a BST
+#
+# https://leetcode.com/problems/delete-node-in-a-bst/description/
+#
+# algorithms
+# Medium (41.23%)
+# Total Accepted:    85.6K
+# Total Submissions: 207.5K
+# Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+#
+# Given a root node reference of a BST and a key, delete the node with the
+# given key in the BST. Return the root node reference (possibly updated) of
+# the BST.
+# 
+# Basically, the deletion can be divided into two stages:
+# 
+# Search for a node to remove.
+# If the node is found, delete the node.
+# 
+# 
+# 
+# Note: Time complexity should be O(height of tree).
+# 
+# Example:
+# 
+# root = [5,3,6,2,4,null,7]
+# key = 3
+# 
+# ⁠   5
+# ⁠  / \
+# ⁠ 3   6
+# ⁠/ \   \
+# 2   4   7
+# 
+# Given key to delete is 3. So we find the node with value 3 and delete it.
+# 
+# One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+# 
+# ⁠   5
+# ⁠  / \
+# ⁠ 4   6
+# ⁠/     \
+# 2       7
+# 
+# Another valid answer is [5,2,6,null,4,null,7].
+# 
+# ⁠   5
+# ⁠  / \
+# ⁠ 2   6
+# ⁠  \   \
+# ⁠   4   7
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def leftmost(self, n):
+        if not n.left: return n
+        return self.leftmost(n.left)
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root: return root 
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left: return root.right
+            elif not root.right: return root.left 
+            else:
+                lm = self.leftmost(root.right)
+                root.val = lm.val 
+                root.right = self.deleteNode(root.right, lm.val)
+        return root 
+
+
+s = Solution()
+
+
--- test.cpp
+++ test.cpp
@@ -1,14 +1,9 @@
 
 #include "aux.cpp"
-#include "1223.dice-roll-simulation.cpp"
+#include "450.delete-node-in-a-bst.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	int n = 3; 
-	vi roll={1,1,1,2,2,3};
-	// n=2, roll={1,1,2,2,2,3};
-	n=5000, roll={13, 3, 12, 14, 11, 11};
-	say(s.dieSimulator(n, roll));
 	return 0;
 }
 
