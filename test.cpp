c4340b1cac22a47250274ef84ad5ef8cfbf52d84
117ami <ssruoz@gmail.com>
2019-12-26 14:37:00 +0800

    p1223 dice roll simulation [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,14 @@
 
 #include "aux.cpp"
-#include "892.surface-area-of-3d-shapes.cpp"
+#include "1223.dice-roll-simulation.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vvi g{{1,2},{3,4}};
-	say(s.surfaceArea(g));
+	int n = 3; 
+	vi roll={1,1,1,2,2,3};
+	// n=2, roll={1,1,2,2,2,3};
+	n=5000, roll={13, 3, 12, 14, 11, 11};
+	say(s.dieSimulator(n, roll));
 	return 0;
 }
 
