e838e705d4dca2ca7de5d297fb0875e5a7c5d159
117ami <ssruoz@gmail.com>
2019-12-27 12:11:02 +0800

    p81-Medium search in rotated sorted array [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1187.make-array-strictly-increasing.cpp"
+#include "81.search-in-rotated-sorted-array-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi a = {5,3,7,8}, b={3,4,5,6};
-	say(s.makeArrayIncreasing(a,b));
 	return 0;
 }
 
