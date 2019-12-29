bdec53832a6665ba3395c947daf8538881879bcf
117ami <ssruoz@gmail.com>
2019-12-29 14:24:12 +0800

    p1302 deepest leaves sum [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
 
 #include "aux.cpp"
-#include "1299.replace-elements-with-greatest-element-on-right-side.cpp"
+#include "1302.deepest-leaves-sum.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	TreeNode* r = new TreeNode(50); 
+	r->right = new TreeNode(54);
+	r->right->left = new TreeNode(98);
+	r->right->right = new TreeNode(6);
+	r->right->right->right = new TreeNode(34);
+	say(s.deepestLeavesSum(r));
 	return 0;
 }
 
