c22f2bc998f421cd1849b563d9c4fe3665b6d354
117ami <ssruoz@gmail.com>
2019-12-30 18:13:35 +0800

    p926-Medium flip string to monotone increasing [CPP/Python]

---
+++ post.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w 
+use v5.18.2;
+
+
+if ($#ARGV == 0 || lc($ARGV[0]) eq 'xiu') {
+    say "^_^ Submint through account : XIU";
+    `cp /Users/alpha/.lc/xiu.json /Users/alpha/.lc/leetcode/user.json `;
+}  elsif ( $#ARGV >= 1 && lc($ARGV[0]) eq '117') {
+    say "^_^ Submint through account : 117";
+    `cp /Users/alpha/.lc/firefly.json /Users/alpha/.lc/leetcode/user.json `;
+} else {
+    die "Choices are either XIU or 117";
+}
+
+say `/usr/local/bin/proxychains4 -q /usr/local/bin/leetcode submit $ARGV[1]`;
--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "990.satisfiability-of-equality-equations.cpp"
+#include "926.flip-string-to-monotone-increasing.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vs eqs = {"z!=b", "a==b", "b!=c", "c==a"};
-	eqs = {"c==c","b==d","x!=z"};
-	say(s.equationsPossible(eqs));
 	return 0;
 }
 
