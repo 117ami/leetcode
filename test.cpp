1ce903297d9c6860b2bd477f9c578e721f65fe03
117ami <ssruoz@gmail.com>
2019-12-31 17:21:40 +0800

    p116-Medium populating next right pointers in each node [CPP/Python[

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 
 #include "aux.cpp"
-#include "1169.invalid-transactions.cpp"
+#include "116.populating-next-right-pointers-in-each-node.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
