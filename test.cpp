5c47542e0a569c7beb15ad96f1ac1c602822a672
117ami <ssruoz@gmail.com>
2020-01-16 14:57:00 +0800

    p480-Medium sliding window median [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1144.decrease-elements-to-make-array-zigzag.cpp"
+#include "850.rectangle-area-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi ns = {9,6,1,6,2};
-	say(s.movesToMakeZigzag(ns));
 	return 0;
 }
 
