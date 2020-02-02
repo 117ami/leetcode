e5c05e1d62454d3a840768aa5690ac8cd6c9f863
117ami <ssruoz@gmail.com>
2020-02-03 07:05:40 +0800

    p1332-Easy remove palindromic subsequence [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1331.rank-transform-of-an-array.cpp"
+#include "1332.remove-palindromic-subsequences.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi arr = {5,1,2,1,3,2};
-	say(s.arrayRankTransform(arr));
 	return 0;
 }
 
