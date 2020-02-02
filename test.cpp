1263f82b8e87da724994aaedfc2b2cdf7c70e5ae
117ami <ssruoz@gmail.com>
2020-02-03 07:20:50 +0800

    p1341-Easy the k-weakest rows in matrix [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 
 #include "aux.cpp"
-#include "1332.remove-palindromic-subsequences.cpp"
+#include "1341.the-k-weakest-rows-in-a-matrix.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string ss = "[[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]]";
+	vvi mat = extractMatrixFromString(ss);
+	say(s.kWeakestRows(mat, 3));
 	return 0;
 }
 
