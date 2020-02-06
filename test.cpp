14eea9f5d94a714fbec88c479202da7e91e1491c
117ami <ssruoz@gmail.com>
2020-02-06 08:42:56 +0800

    p1333-Medium filter restaurants by vegan [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 
 #include "aux.cpp"
-#include "1335.minimum-difficulty-of-a-job-schedule.cpp"
+#include "1333.filter-restaurants-by-vegan-friendly-price-and-distance.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi jobs = {11, 111, 22, 222, 33, 333, 44, 444}; int d = 6; 
-	say(s.minDifficulty(jobs, d));
+	vvi rest = extractMatrixFromString("[[1,4,1,40,10],[2,8,0,50,5],[3,8,1,30,4],[4,10,0,10,3],[5,1,1,15,1]]"); 
+	say(s.filterRestaurants(rest, 0, 50, 10));
 	return 0;
 }
 
