e7ca5bdd9784152d65fbc1d49f22ec17347809f5
117ami <ssruoz@gmail.com>
2020-02-10 07:06:29 +0800

    p1346-Easy check if n and its double [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 
 #include "aux.cpp"
-#include "1342.number-of-steps-to-reduce-a-number-to-zero.cpp"
+#include "1346.check-if-n-and-its-double-exist.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.numberOfSteps(123));
+	vi arr = {0, 0};
+	say(s.checkIfExist(arr));
 	return 0;
 }
 
