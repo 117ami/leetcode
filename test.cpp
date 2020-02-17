903d63e1ded6d8bbcb7197999e6967acdb8d6c50
117ami <ssruoz@gmail.com>
2020-02-18 07:53:33 +0800

    p1352-Medium product of the last k number [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,13 +1,9 @@
 
 #include "aux.cpp"
-#include "1354.construct-target-array-with-multiple-sums.cpp"
+#include "1352.product-of-the-last-k-numbers.cpp"
 
 int main(int argc, char const *argv[]) {
-	Solution s;
-	vi target = {3,5,9};
-	target = {1,49,11,1,25,1,7};
-	target = {9,3,5};
-	say(s.isPossible(target));
+	ProductOfNumbers p; 
 	return 0;
 }
 
