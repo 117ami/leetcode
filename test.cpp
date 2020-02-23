4173b2f74cee5b237927808db3421d91384da331
117ami <ssruoz@gmail.com>
2020-02-23 08:09:32 +0800

    p1356-Easy sort integers by the number of 1 bits [CPP/Python]

--- test.cpp
+++ test.cpp
@@ -1,25 +1,12 @@
 
 #include "aux.cpp"
-#include "699.falling-squares.cpp"
+#include "1356.sort-integers-by-the-number-of-1-bits.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi pos = {1,2,3,5,6,8,9};
-	auto i = upper_bound(pos.begin(), pos.end(), 4);
-    auto j = lower_bound(pos.begin(), pos.end(), 7);
-    int h = *max_element(i, j);
-	vvi positions = {{1,2}, {2,3}, {6,1}};
-	say(s.fallingSquares(positions));
-
-	map<int, int> mp; 
-	mp[0] = 0; 
-	mp[2] = 2; 
-	mp[4] = 4; 
-	mp[5] = 5; 
-	mp[1] = 1; 
-	mp[3] = 3; 
-	// for (auto it: mp) say(vi{it.first, it.second});
-	
+	vi arr = {1,2,3,4,5,6,7,8};
+	arr = {1024,512,256,128,64,32,16,8,4,2,1};
+	say(s.sortByBits(arr));
 	return 0;
 }
 
