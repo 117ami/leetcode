843e4e39a515fa0d44a45880ce86d13325992a1a
117ami <ssruoz@gmail.com>
2020-02-24 07:21:58 +0800

    p1361-Medium validate binary tree nodes [C++/Python]

--- python_solutions/1361.validate-binary-tree-nodes.cpp
+++ python_solutions/1361.validate-binary-tree-nodes.cpp
@@ -760,4 +760,98 @@ int rdn(int year, int month, int day) { /* Rata Die day one is 0001-01-01 */
 }
 
 
-  
+  /*
+ * @lc app=leetcode id=1361 lang=cpp
+ *
+ * [1361] Validate Binary Tree Nodes
+ *
+ * https://leetcode.com/problems/validate-binary-tree-nodes/description/
+ *
+ * algorithms
+ * Medium (69.16%)
+ * Total Accepted:    4.7K
+ * Total Submissions: 6.7K
+ * Testcase Example:  '4\n[1,-1,3,-1]\n[2,-1,-1,-1]'
+ *
+ * You have n binary tree nodes numbered from 0 to n - 1 where node i has two
+ * children leftChild[i] and rightChild[i], return true if and only if all the
+ * given nodes form exactly one valid binary tree.
+ * 
+ * If node i has no left child then leftChild[i] will equal -1, similarly for
+ * the right child.
+ * 
+ * Note that the nodes have no values and that we only use the node numbers in
+ * this problem.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,-1,-1,-1]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,3,-1,-1]
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 2, leftChild = [1,0], rightChild = [-1,-1]
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 6, leftChild = [1,-1,-1,4,-1,-1], rightChild = [2,-1,-1,5,-1,-1]
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 10^4
+ * leftChild.length == rightChild.length == n
+ * -1 <= leftChild[i], rightChild[i] <= n - 1
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& lc, vector<int>& rc) {
+        unordered_set<int> seen; 
+        vector<pair<int, int>> children; 
+        for(int i = 0; i < rc.size(); i++) children.push_back(make_pair(lc[i], rc[i]));
+        stack<int> jobs; jobs.push(0);
+
+        while (!jobs.empty()) {
+          int cur = jobs.top(); jobs.pop(); 
+          if (seen.find(cur) != seen.end()) return false; 
+          seen.insert(cur);
+
+          pair<int, int> pi = children[cur];
+          if (pi.first >= 0) jobs.push(pi.first);
+          if (pi.second >= 0) jobs.push(pi.second);
+        }
+
+        return seen.size() == n; 
+    }
+};
+
+
+
+static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL);return 0; }();
