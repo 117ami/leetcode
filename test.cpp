8d4014464e5fd9a8b2c8925ba5c1e524a3cd1622
117ami <ssruoz@gmail.com>
2020-03-12 13:33:01 +0800

    p1379-M find a corresponding node [C++/Python]

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 
 #include "aux.cpp"
-#include "1370.increasing-decreasing-string.cpp"
+#include "1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.sortString("aaaabbbbcccc"));
 	return 0;
 }
 
