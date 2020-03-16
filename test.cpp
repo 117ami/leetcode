a06cdafaf411fcc89f53c8cabe83293c7c5337a2
117ami <ssruoz@gmail.com>
2020-03-16 18:13:17 +0800

    p1380-E lucky numbers in a matrix [C++/Python]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "1382.balance-a-binary-search-tree.cpp"
+#include "1380.lucky-numbers-in-a-matrix.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi x = {1,INT_MAX,2,INT_MAX,3,INT_MAX,4,INT_MAX,INT_MAX};
-	TreeNode* r = growTreeFromList(x);
-	s.balanceBST(r);
 	return 0;
 }
 
