02f7d24cbac4ea781141d12baba45a0ba2771961
117ami <ssruoz@gmail.com>
2020-03-21 13:01:35 +0800

    p1381-M design a stack with increment operation [C++/Python]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 
 #include "aux.cpp"
-#include "1380.lucky-numbers-in-a-matrix.cpp"
+#include "1381.design-a-stack-with-increment-operation.cpp"
 
 int main(int argc, char const *argv[]) {
-	Solution s;
+	
 	return 0;
 }
 
