8fd5e78a24cc8fa47d262b3096ad7ee678c3406a
117ami <ssruoz@gmail.com>
2020-03-24 10:26:49 +0800

    p1385-E find the distance value between two arrays [C++/Python]

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 
 #include "aux.cpp"
-#include "1387.sort-integers-by-the-power-value.cpp"
+#include "1385.find-the-distance-value-between-two-arrays.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.getKth(1,1000,777));
+	vi a = {4,5,8} ,b={10,9,1,8}; int d=2; 
+	say(s.findTheDistanceValue(a,b,d));
 	return 0;
 }
 
