054b8c1f3c7c862645ec4da8f6a637edeceff434
117ami <ssruoz@gmail.com>
2020-04-07 15:13:57 +0800

    p1402-Hard reducing dishes [C++/Python]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
 
 #include "aux.cpp"
-#include "1403.minimum-subsequence-in-non-increasing-order.cpp"
+#include "1402.reducing-dishes.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi ns = {4,3,10,9,8}; 
-	ns = {6, 3, 3};
-	say(s.minSubsequence(ns));
+	vi sa = {3,4,2};
+	sa = {-1,-8,0,5,-9};
+	sa = {-2,5,-1,0,3,-3};
+	say(s.maxSatisfaction(sa));
 	return 0;
 }
 
