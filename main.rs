8dba99cc7810fcc5ef4f8577969d44cd17075db8
117ami <ssruoz@gmail.com>
2020-04-28 12:57:19 +0800

    p337-Medium house robber iii [Rust/Python3]

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
 
-mod question; 
-
+use std::cmp::max; 
 fn main(){
-	let matrix = vec![vec![1,2,3], vec![4,5,6], vec![7,8,9]];
-	println!("{:?}", question::Solution::find_diagonal_order(matrix));
+	// println!("{:?}", question::Solution::fn());
+	let v = vec![1, 2];
+	println!("{:?}", v.iter().max().unwrap());
 }
 
---
+++ python_solutions/337.house-robber-iii.py
@@ -0,0 +1,85 @@
+from collections import Counter, defaultdict, OrderedDict, deque
+from bisect import bisect_left, bisect_right 
+from functools import reduce, lru_cache 
+from typing import List 
+import itertools 
+import math 
+import string
+true = True
+false = False
+MIN, MAX = -0x3f3f3f3f, 0x3f3f3f3f
+#
+# @lc app=leetcode id=337 lang=python3
+#
+# [337] House Robber III
+#
+# https://leetcode.com/problems/house-robber-iii/description/
+#
+# algorithms
+# Medium (50.02%)
+# Total Accepted:    143.5K
+# Total Submissions: 286.7K
+# Testcase Example:  '[3,2,3,null,3,null,1]'
+#
+# The thief has found himself a new place for his thievery again. There is only
+# one entrance to this area, called the "root." Besides the root, each house
+# has one and only one parent house. After a tour, the smart thief realized
+# that "all houses in this place forms a binary tree". It will automatically
+# contact the police if two directly-linked houses were broken into on the same
+# night.
+# 
+# Determine the maximum amount of money the thief can rob tonight without
+# alerting the police.
+# 
+# Example 1:
+# 
+# 
+# Input: [3,2,3,null,3,null,1]
+# 
+# ⁠    3
+# ⁠   / \
+# ⁠  2   3
+# ⁠   \   \ 
+# ⁠    3   1
+# 
+# Output: 7 
+# Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+# 
+# Example 2:
+# 
+# 
+# Input: [3,4,5,1,3,null,1]
+# 
+# 3
+# ⁠   / \
+# ⁠  4   5
+# ⁠ / \   \ 
+# ⁠1   3   1
+# 
+# Output: 9
+# Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+# 
+#
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def rec(node):
+            if not node: return (0, 0)
+            lv, rv = rec(node.left), rec(node.right)
+            rob = norob = 0
+            rob = node.val + lv[1] + rv[1]
+            norob = max(lv) + max(rv)
+            return (rob, norob)
+
+        return max(rec(root))
+        
+
+sol = Solution()
+
+
