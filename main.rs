d469407e4ea99052b91ce7561c2a0d5d5f0e01c6
117ami <ssruoz@gmail.com>
2020-05-11 16:53:39 +0800

    p733-Easy flood fill [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let mat = [[1,10,10],[1,4,5],[2,3,6]].iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
-	println!("{:?}", question::Solution::kth_smallest(mat, 12));
+	let ns = [[1, 1, 1], [1, 1, 0], [1, 0, 1]].iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
+	println!("{:?}", question::Solution::flood_fill(ns, 1, 1, 2));
 }
 
