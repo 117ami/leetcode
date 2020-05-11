6ae2a7eb8992466c5fa3d7487c0ebb1f2f25fb1b
117ami <ssruoz@gmail.com>
2020-05-11 14:10:30 +0800

    p1439-Hard find kth smallest sum of matrix [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,6 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::kth_smallest());
+	let mat = [[1,10,10],[1,4,5],[2,3,6]].iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
+	println!("{:?}", question::Solution::kth_smallest(mat, 12));
 }
 
