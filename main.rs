23f0ea13b47a3427d68fd02f37e3a4045583ca73
117ami <ssruoz@gmail.com>
2020-05-13 18:07:36 +0800

    p28-Easy implement strstr [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,8 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let ns = "1000".to_string(); 
-	println!("{:?}", question::Solution::remove_kdigits(ns, 3));
+	let h = "abc".to_string(); 
+	let n = "bc".to_string();
+	println!("{:?}", question::Solution::str_str(h, n));
 }
 
--- python_solutions/28.implement-strstr.py
+++ python_solutions/28.implement-strstr.py
@@ -1,3 +1,14 @@
+from collections import Counter, defaultdict, OrderedDict, deque
+from bisect import bisect_left, bisect_right 
+from functools import reduce, lru_cache 
+from typing import List 
+import itertools 
+import math 
+import heapq 
+import string
+true = True
+false = False
+MIN, MAX = -0x3f3f3f3f, 0x3f3f3f3f
 #
 # @lc app=leetcode id=28 lang=python3
 #
@@ -6,9 +17,9 @@
 # https://leetcode.com/problems/implement-strstr/description/
 #
 # algorithms
-# Easy (30.72%)
-# Total Accepted:    359.1K
-# Total Submissions: 1.2M
+# Easy (34.09%)
+# Total Accepted:    628.8K
+# Total Submissions: 1.8M
 # Testcase Example:  '"hello"\n"ll"'
 #
 # Implement strStr().
@@ -39,23 +50,44 @@
 # string. This is consistent to C's strstr() and Java's indexOf().
 # 
 #
+class KMP():
+    def get_lhp(self, t: str) -> List[int]:
+        '''Compute the length of LHP for each t[:i], i \in [1..len(t)],
+        where a prefix-suffix of t is a substring, u, of t s.t., t.startswith(u) and t.endswith(u).
+        And proper means, len(u) < len(t), i.e., u != t
+        '''
+        j, lhp = 0, [0] * len(t)
+        for i in range(1, len(t)):
+            while j > 0 and t[i] != t[j]:
+                j = lhp[j-1]
+                
+            if t[i] == t[j]:
+                j += 1
+                lhp[i] = j
+        return lhp
+
+    def pattern_search(self, text: str, pat: str) -> List[int]:
+        """KMP (Knuth Morris Pratt) Pattern Searching
+        Return a list of indexes i, such that t occurs in s starting from i.
+        """
+        j = 0
+        lhp, res = self.get_lhp(pat), []
+        for i in range(len(text)):
+            while j > 0 and text[i] != pat[j]:
+                j = lhp[j-1]
+
+            if text[i] == pat[j]:
+                j += 1 
+
+            if j == len(pat):
+                res.append(i + 1 - len(pat))
+                j = lhp[j - 1]
+        return res
+
 class Solution:
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if not needle: return 0
-        for i in range(len(haystack) - len(needle) + 1):
-        	if haystack[i:].startswith(needle):
-        		return i
-        return -1
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)        
 
-haystack = "hello"
-needle = "ll"
-print(Solution().strStr(haystack, needle))
+sol = Solution()
+print(sol.strStr("a", "0"))
 
-
-
-        
