c159c663c8c17b83ee32c0b8bd7e9adff5bc363a
117ami <ssruoz@gmail.com>
2020-05-17 13:37:51 +0800

    p1446-Easy consecutive characters

--- main.rs
+++ main.rs
@@ -4,15 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let mut obj = question::Trie::new();
-	obj.insert("apple".to_string());
-	println!("{:?}", obj.prefixes);
-
-	println!("{}", obj.search("apple".to_string()));
-	println!("{}", obj.search("app".to_string()));
-	println!("{}", obj.starts_with("app".to_string()));
-	obj.insert("app".to_string());
-	println!("{}", obj.search("app".to_string()));
-
+	let s = "aaabcaabcdaaa".to_string();
+	println!("{:?}", question::Solution::max_power(s));
 }
 
