feb5edc6292ff04b5be960eeb9c3e9846cd8d248
117ami <ssruoz@gmail.com>
2020-05-17 14:28:52 +0800

    p1450-Easy number of students doing hw [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,8 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let s = "aaabcaabcdaaa".to_string();
-	println!("{:?}", question::Solution::max_power(s));
+	let st = vec![9,8,7,6,5,4,3,2,1]; 
+	let et = vec![10,10,10,10,10,10,10,10,10]; 
+	println!("{:?}", question::Solution::busy_student(st, et, 5));
 }
 
