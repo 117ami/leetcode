360a1fae847db641e62154f65389bec9d23876cd
117ami <ssruoz@gmail.com>
2020-05-18 15:39:56 +0800

    p567-Medium premutation in string [Rust/Python3]

--- main.rs
+++ main.rs
@@ -3,16 +3,9 @@ mod question;
 
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
-fn capitalize(word: String) -> String { 
-	let mut res = word;
-	res.chars().take(1).flat_map(char::to_uppercase).chain(res.chars().skip(1)).collect()
-}
-
 fn main(){
-	println!("{:?}", question::Solution::arrange_words("Glad to see you".to_string()));
-	let mut fw = "lowercase".to_string();
-	// fw = fw.chars().take(1).flat_map(char::to_uppercase).chain(fw.chars().skip(1)).collect(); 
-	// fw = fw.chars().skip(1).collect();
-	println!("{:?}", capitalize("apple".to_string()) );
+	let s = "ab".to_string();
+	let t = "bcooooazbccc".to_string();
+	println!("{:?}", question::Solution::check_inclusion(s, t));
 }
 
