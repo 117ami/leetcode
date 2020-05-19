e6a0ff97c4fa36029f84612c32c70eecc205468a
117ami <ssruoz@gmail.com>
2020-05-19 16:50:25 +0800

    p1452-Easy subset [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,8 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let s = "ab".to_string();
-	let t = "bcooooazbccc".to_string();
-	println!("{:?}", question::Solution::check_inclusion(s, t));
+	let fc = vec![vec!["leetcode","google","facebook"],vec!["google","microsoft"],vec!["google","facebook"],vec!["google"],vec!["amazon"]].iter().map(|c| c.iter().map(|z| z.to_string()).collect() ).collect::<Vec<Vec<_>>>();
+	println!("{:?}", question::Solution::people_indexes(fc));
 }
 
