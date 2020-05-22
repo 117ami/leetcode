130e233414660f8aadc0e07226f7997380d44d8b
117ami <ssruoz@gmail.com>
2020-05-22 14:41:54 +0800

    p393-Medium utf8 validation [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,6 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let ns = vec![1,1,2,2,2,2];
-	println!("{:?}", question::Solution::three_sum_multi(ns, 5));
+	println!("{:?}", question::Solution::valid_utf8(vec![255]));
 }
 
