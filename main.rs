8d35d06987c374c1b323e108138a16b891f0ebcf
117ami <ssruoz@gmail.com>
2020-05-24 22:40:51 +0800

    p1455-Easy check if a word occurs [Rust/Python3]

--- main.rs
+++ main.rs
@@ -4,6 +4,8 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::valid_utf8(vec![255]));
+	let s = "i use triple pillow".to_string(); 
+	let t= "pil".to_string();
+	println!("{:?}", question::Solution::is_prefix_of_word(s, t));
 }
 
