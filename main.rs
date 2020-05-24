585bc4c50390e1f1c78b35d0ed34ea7376c49943
117ami <ssruoz@gmail.com>
2020-05-25 01:41:22 +0800

    p1456-Medium [Rust/Python3/C++]

--- main.rs
+++ main.rs
@@ -4,6 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::fn());
+	let s = "leetcode".to_string(); 
+	println!("{:?}", question::Solution::max_vowels(s, 2));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 
 #include "aux.cpp"
-#include "1457.pseudo-palindromic-paths-in-a-binary-tree.cpp"
+#include "1456.maximum-number-of-vowels-in-a-substring-of-given-length.cpp"
 
 int main(int argc, char const *argv[]) {
-	// Solution s;
-	vector<int> cnt(10, 0);
+	Solution s;
+	say(s.maxVowels("leetcode", 3));
 	return 0;
 }
 
