d5a313df74a90226bfc37fd3fe5ef9367b573a7a
117ami <ssruoz@gmail.com>
2020-06-01 09:46:55 +0800

    p1460-Easy make two arrays equal by reversing sub arrays [Rust/Python3/C++]

--- main.rs
+++ main.rs
@@ -4,6 +4,8 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::solve_sudoku());
+	let a = vec![1,3,2];
+	let b = vec![9,3,1];
+	println!("{:?}", question::Solution::can_be_equal(a, b));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 
 #include "aux.cpp"
-#include "37.sudoku-solver.cpp"
+#include "1460.make-two-arrays-equal-by-reversing-sub-arrays.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
