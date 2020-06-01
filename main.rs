74338e759a72d95331f4133a92ff16e01f4df468
117ami <ssruoz@gmail.com>
2020-06-01 20:01:53 +0800

    p1461-Medium check if a string contains all binary codes [Rust/C++/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let tmp = vec![vec![0, 1], vec![3, 2], vec![4,3], vec![1,4]];
-	println!("{:?}", question::Solution::min_reorder(5, tmp));
+	let s = "0010101".to_string();
+	println!("{:?}", question::Solution::has_all_codes(s, 2));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 
 #include "aux.cpp"
-#include "1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp"
+#include "1461.check-if-a-string-contains-all-binary-codes-of-size-k.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<vector<int>> tmp = {{1, 0}, {0, 2}};
-	say(s.minReorder(3, tmp));
+	say(s.hasAllCodes("0000000001011100", 4));
 	return 0;
 }
 
