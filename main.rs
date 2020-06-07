5e605e7e158aeb3ce0acb2224967fe3cc041c938
117ami <ssruoz@gmail.com>
2020-06-07 21:22:52 +0800

    p406-Medium queue reconstruction by height

--- main.rs
+++ main.rs
@@ -1,10 +1,11 @@
 
-mod question; 
-
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let people = vec![vec![7,0], vec![4,4], vec![7,1], vec![5,0], vec![6,1], vec![5,2]];
-	println!("{:?}", question::Solution::reconstruct_queue(people));
+	let arr = vec![1,2,3,4];
+	for n in arr.iter() {
+		println!("{}", n);
+	}
+	println!("{:?}", arr);
 }
 
