28b9a42050a260c9da08a2abeb830c216ca8b35f
117ami <ssruoz@gmail.com>
2020-06-07 21:55:08 +0800

    p1470-Easy shuffle the array [Rust/C++/Python3]

--- main.rs
+++ main.rs
@@ -1,11 +1,10 @@
 
+mod question; 
+
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let arr = vec![1,2,3,4];
-	for n in arr.iter() {
-		println!("{}", n);
-	}
-	println!("{:?}", arr);
+	let x = vec![2,3,4,5,6,7]; 
+	println!("{:?}", question::Solution::shuffle(x, 3));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "406.queue-reconstruction-by-height.cpp"
+#include "1470.shuffle-the-array.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<vector<int>> p = {{5,0}, {4, 1}};
-	vector<vector<int>> res = s.reconstructQueue(p);
-	say(res);
 	return 0;
 }
 
