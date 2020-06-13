3da87a475bd3f5ec3b0db92a22f0eb3990d47c5d
117ami <ssruoz@gmail.com>
2020-06-13 15:11:22 +0800

    p1471-Medium the k strongest values in an array [Rust/Python3/C++]

--- main.rs
+++ main.rs
@@ -4,8 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let bh = question::BrowserHistory{pos: 0, h: vec![]};
-	//  {"Leetcode.com".to_string()}	;
-	
+	let v = vec![6,7,11,7,6,8, 100_000];
+	println!("{:?}", question::Solution::get_strongest(v, 5));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 
 #include "aux.cpp"
-#include "1472.design-browser-history.cpp"
+#include "1471.the-k-strongest-values-in-an-array.cpp"
 
 int main(int argc, char const *argv[]) {
-	BrowserHistory bh("lc");
-	bh.visit("google");
+	Solution s;
+	vector<int> x = {6,7,11,7,6,8};
+	say(s.getStrongest(x, 4));
 	return 0;
 }
 
