6e1eacbe536c7a9a01cfcb33e6dad8e26fbd6744
117ami <ssruoz@gmail.com>
2020-06-14 01:04:11 +0800

    p1476-Easy subrectangle queries [Rust/Python/C++]

--- main.rs
+++ main.rs
@@ -4,7 +4,6 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let a = vec![8,4,6,2,3];
-	println!("{:?}", question::Solution::final_prices(a));
+	println!("{:?}", question::Solution::Vec<Vec<i32>>)());
 }
 
--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1475.final-prices-with-a-special-discount-in-a-shop.cpp"
+#include "1476.subrectangle-queries.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> a = {8,4,6,2,3};
-	say(s.finalPrices(a));
 	return 0;
 }
 
