510347a6adfdab84acf7bf34a14d3ca63257c367
GaoangLiu <ssruoz@gmail.com>
2020-06-18 03:25:17 +0800

    p66-E plus one [C++/Python/Rust]

--- main.rs
+++ main.rs
@@ -4,8 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let ws = vec!["word".to_string(), "world".into(), "row".into()];
-	let o = "worldabcefghijkmnpqstuvxyz".to_string();
-	println!("{:?}", question::Solution::is_alien_sorted(ws, o));
+	let x = vec![9,9,9];
+	println!("{:?}", question::Solution::plus_one(x));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
 
 #include "aux.cpp"
-#include "953.verifying-an-alien-dictionary.cpp"
+#include "66.plus-one.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vs words = {"word","world","row"} ;
-	string order = "worldabcefghijkmnpqstuvxyz";
-	say(s.isAlienSorted(words, order));
+	vi x= {9,9,9};
+	say(s.plusOne(x));
+
+
 	return 0;
 }
 
