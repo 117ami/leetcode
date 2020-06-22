3539118e76ee580c83952acdef3c56a4914e9d27
GaoangLiu <ssruoz@gmail.com>
2020-06-22 11:24:49 +0800

    p1487-Medium making file names unique [Rust/C++/Python3]

--- main.rs
+++ main.rs
@@ -4,6 +4,8 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::get_folder_names());
+	// let x = vec!["gta".to_string(),"gta(1)".into(),"gta".into(),"avalon".into()]; 
+	let x = vec!["wano".to_string(),"wano".into(),"wano".into(),"wano".into()];
+	println!("{:?}", question::Solution::get_folder_names(x	));
 }
 
--- test.cpp
+++ test.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vs x = {"wano","wano","wano","wano"};
+	x = {"gta","gta(1)","gta","avalon"};
+	say(s.getFolderNames(x));
 	return 0;
 }
 
