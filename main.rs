9e8856bc7f938f9a89c8101148afb9bafd20c270
GaoangLiu <ssruoz@gmail.com>
2020-06-23 10:25:08 +0800

    p136-Easy single number [C++/Python/Rust]

--- main.rs
+++ main.rs
@@ -4,6 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::single_number());
+	let v = vec![1,2,3,2,1];
+	println!("{:?}", question::Solution::single_number(v));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "137.single-number-ii.cpp"
+#include "136.single-number.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	vi x = {1,2,3,2,1};
+	say(s.singleNumber(x));
 	return 0;
 }
 
