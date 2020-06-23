a99b8b31c011d89b53507155681a1c9aa6eaf1d8
GaoangLiu <ssruoz@gmail.com>
2020-06-23 10:13:37 +0800

    p137-Medium single number ii [C++/Python/Rust]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "1487.making-file-names-unique.cpp"
+#include "137.single-number-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vs x = {"wano","wano","wano","wano"};
-	x = {"gta","gta(1)","gta","avalon"};
-	say(s.getFolderNames(x));
 	return 0;
 }
 
