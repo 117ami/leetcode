ed52a4e43947260234146bbc89bb8c87b9885749
GaoangLiu <ssruoz@gmail.com>
2020-06-28 14:36:24 +0800

    p1496-Easy path crossing [Rust/Python3/C++]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "1492.the-kth-factor-of-n.cpp"
+#include "1496.path-crossing.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string xxx = "ESN";
+	say(s.isPathCrossing(xxx));
 	return 0;
 }
 
