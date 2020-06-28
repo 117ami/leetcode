585be39896acc2c8187a225077ef53c6b15d8396
GaoangLiu <ssruoz@gmail.com>
2020-06-28 09:59:22 +0800

    p1492-Medium the kth factor of n [C++/Python3/Rust]

--- main.rs
+++ main.rs
@@ -4,7 +4,6 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let s = vec![1,2,3,4];
-	println!("{:?}", question::Solution::average(s));
+	println!("{:?}", question::Solution::kth_factor(1000, 3));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1491.average-salary-excluding-the-minimum-and-maximum-salary.cpp"
+#include "1492.the-kth-factor-of-n.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi sa = {1, 2, 3, 4};
-	say(s.average(sa));
 	return 0;
 }
 
