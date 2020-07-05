2580ef0bcb0812856a1eae46a28d08a723425e67
GaoangLiu <ssruoz@gmail.com>
2020-07-05 12:56:31 +0800

    p1502-Medium can make arithmetic [Rust/C++/Python3]

--- main.rs
+++ main.rs
@@ -4,6 +4,7 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	println!("{:?}", question::Solution::prison_after_n_days());
+	let arr = vec![1,3,5];
+	println!("{:?}", question::Solution::can_make_arithmetic_progression(arr));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,15 +1,9 @@
 
 #include "aux.cpp"
-#include "957.prison-cells-after-n-days.cpp"
+#include "1502.can-make-arithmetic-progression-from-sequence.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi x = {1,0,0,1,0,0,1,0}; int N = 1000000000;
-	say(s.prisonAfterNDays(x, N));
-	string xxs = bitset<8>(12).to_string(); 
-	vector<char> vcs (xxs.begin(), xxs.end());
-	
-	say(vcs);
 	return 0;
 }
 
