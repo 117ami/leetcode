476ab8b6b203b83ef28ef93350046de0863c6da7
GaoangLiu <ssruoz@gmail.com>
2020-07-13 16:56:12 +0800

    p1512-Easy number of good pairs [C++/Python3/Rust]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1508.range-sum-of-sorted-subarray-sums.cpp"
+#include "1512.number-of-good-pairs.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi x={1,2,3,4};
-	say(s.rangeSum(x, 4, 1, 5));
 	return 0;
 }
 
