b7d20b431b7b39ec7a3c6f2ed293a5738ac59ca0
GaoangLiu <ssruoz@gmail.com>
2020-07-14 15:19:11 +0800

    p866-Medium prime palindrome [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 
 #include "aux.cpp"
-#include "1512.number-of-good-pairs.cpp"
+#include "866.prime-palindrome.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	say(s.primePalindrome(6));
 	return 0;
 }
 
