a34f216fc91bec81ab05d33bee5fdf0cafe5dbd8
GaoangLiu <ssruoz@gmail.com>
2020-07-22 20:38:49 +0800

    p103-Easy binary tree zigzag level order traversal [C++/Python3]

--- main.rs
+++ main.rs
@@ -4,7 +4,6 @@ mod question;
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
 fn main(){
-	let x = vec![9, 12, 3];
-	println!("{:?}", question::Solution::closest_to_target(x, 5));
+	println!("{:?}", question::Solution::closest_to_target());
 }
 
--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1521.find-a-value-of-a-mysterious-function-closest-to-target.cpp"
+#include "103.binary-tree-zigzag-level-order-traversal.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi ip = {9, 12, 3, 7, 15};
-	say(s.closestToTarget(ip, 5));
 	return 0;
 }
 
