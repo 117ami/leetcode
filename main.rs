bc4c214d62633e4c789a48f3653a4cc9f53488da
GaoangLiu <ssruoz@gmail.com>
2020-07-22 15:16:32 +0800

    p1521-Hard find a value of a mysterious function [C++/Rust/Python3]

--- main.rs
+++ main.rs
@@ -3,7 +3,8 @@ mod question;
 
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
-fn main(){	
-	println!("{:?}", question::Solution::num_sub("011".to_string()));
+fn main(){
+	let x = vec![9, 12, 3];
+	println!("{:?}", question::Solution::closest_to_target(x, 5));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 
 #include "aux.cpp"
-#include "1513.number-of-substrings-with-only-1s.cpp"
+#include "1521.find-a-value-of-a-mysterious-function-closest-to-target.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.numSub("0110111"));
+	vi ip = {9, 12, 3, 7, 15};
+	say(s.closestToTarget(ip, 5));
 	return 0;
 }
 
