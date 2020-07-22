97d321cc6d190a2bc7f7eb68e00f0fbc546dfc6f
GaoangLiu <ssruoz@gmail.com>
2020-07-22 14:13:06 +0800

    p1513-M number of substrings with only 1s [C++/Python3/Rust]

--- main.rs
+++ main.rs
@@ -3,7 +3,7 @@ mod question;
 
 // let vg = grid.iter().map(|c| c.to_vec()).collect::<Vec<Vec<_>>>();
 
-fn main(){
-	println!("{:?}", question::Solution::exist());
+fn main(){	
+	println!("{:?}", question::Solution::num_sub("011".to_string()));
 }
 
--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 
 #include "aux.cpp"
-#include "79.word-search.cpp"
+#include "1513.number-of-substrings-with-only-1s.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	say(s.numSub("0110111"));
 	return 0;
 }
 
