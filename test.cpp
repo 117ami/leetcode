2ceb8b4322a6c4adc20c3d924303050818d7cd8b
GaoangLiu <ssruoz@gmail.com>
2020-07-25 16:43:33 +0800

    p1509-M minimum differences [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 
 #include "aux.cpp"
-#include "1510.stone-game-iv.cpp"
+#include "1509.minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	forloopup(i, 1, 18)
-		say(s.winnerSquareGame(i));
+	vi x = {1,4,2,28,88};
+	say(s.minDifference(x));
 	return 0;
 }
 
