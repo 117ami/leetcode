aa0de4ecff2cb2865ae2f56bc706a76a4a6aaf49
GaoangLiu <ssruoz@gmail.com>
2020-07-26 23:20:25 +0800

    p1528-E shuffle string [C++/Python3/Rust]

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
 
 #include "aux.cpp"
-#include "1523.count-odd-numbers-in-an-interval-range.cpp"
+#include "1528.shuffle-string.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	say(s.countOdds(3, 7));
+	string t = "codeleet";
+	vi indices = {4, 5, 6, 7, 0, 2, 1, 3};
+	say(s.restoreString(t, indices));
 	return 0;
 }
 
