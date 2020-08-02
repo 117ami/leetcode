60a0d0968b8de856c96c233a49b84385c0d81750
GaoangLiu <ssruoz@gmail.com>
2020-08-02 08:24:08 +0800

    p1529-M bulb switcher iv [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp"
+#include "1529.bulb-switcher-iv.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi target = {3,1,5,4,2};
-	say(s.minNumberOperations(target));
 	return 0;
 }
 
