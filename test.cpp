3e88613775ce530b0b6f828876fe725b71c012f8
GaoangLiu <ssruoz@gmail.com>
2020-08-06 21:47:58 +0800

    p442-Medium find all duplicates in an array [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "211.add-and-search-word-data-structure-design.cpp"
+#include "442.find-all-duplicates-in-an-array.0.cpp"
 
 int main(int argc, char const *argv[]) {
-	WordDictionary wd; 
-	wd.addWord("pad");
-	say(wd.search("pxd"));
+	Solution s;
 	return 0;
 }
 
