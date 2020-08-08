155c58d4d7db943a8159d4f788392105ef9f979d
GaoangLiu <ssruoz@gmail.com>
2020-08-08 09:41:44 +0800

    p1520-Medium maximum number of non overlapping substrings [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 
 #include "aux.cpp"
-#include "442.find-all-duplicates-in-an-array.0.cpp"
+#include "1520.maximum-number-of-non-overlapping-substrings.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string st = "bbcacbaba";
+	st ="adefaddaccc";
+	say(s.maxNumOfSubstrings(st));
 	return 0;
 }
 
