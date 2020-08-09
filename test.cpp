8c7fa1ff5ece7650fd8cd7304fa803d861355ecc
GaoangLiu <ssruoz@gmail.com>
2020-08-09 09:36:17 +0800

    p1539-Easy kth-missing positive number [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 
 #include "aux.cpp"
-#include "1524.number-of-sub-arrays-with-odd-sum.cpp"
+#include "1539.kth-missing-positive-number.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi a = {1,2,3,4,5,6,7};
-	say(s.numOfSubarrays(a));
+	vi a = {1,2}; int k  =5; 
+	say(s.findKthPositive(a, k));
 	return 0;
 }
 
