fb48cdc614cd01ff48af9ada18e3bae42a3b42fc
GaoangLiu <ssruoz@gmail.com>
2020-08-10 15:27:03 +0800

    p171-Easy excel sheet column number [C++] added

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 
 #include "aux.cpp"
-#include "1536.minimum-swaps-to-arrange-a-binary-grid.cpp"
+#include "171.excel-sheet-column-number.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
