19374dc5ba33ef36085e3e147ab6910a6eb4ebe3
GaoangLiu <ssruoz@gmail.com>
2020-08-10 16:28:03 +0800

    p1531-Hard string compression ii [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "171.excel-sheet-column-number.cpp"
+#include "1531.string-compression-ii.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string a = "aaaaaaa"; 
+	say(s.getLengthOfOptimalCompression(a, 0));
 	return 0;
 }
 
