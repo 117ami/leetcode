7a9826a0223f1e7d83a26606658c04276c0833da
GaoangLiu <ssruoz@gmail.com>
2020-08-10 11:11:56 +0800

    p1542-Hard find longest awesome substring [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,11 @@
 
 #include "aux.cpp"
-#include "1546.maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp"
+#include "1542.find-longest-awesome-substring.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi ns = {-2,6,6,3,5,4,1,2,8}; 
-	int n = 10; 
-	say(s.maxNonOverlapping(ns, n));
+	string ss = "121234";
+	say(s.longestAwesome(ss));
 	return 0;
 }
 
