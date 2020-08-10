75596965269ebe1ad2e6cfc2fb0c099ce803d95c
GaoangLiu <ssruoz@gmail.com>
2020-08-10 12:38:39 +0800

    p1540-Medium can convert string in k moves [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 
 #include "aux.cpp"
-#include "1371.find-the-longest-substring-containing-vowels-in-even-counts.cpp"
+#include "1540.can-convert-string-in-k-moves.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
+	string a = "input", b="ouput"; 
+	say(s.canConvertString(a, b, 10));
 	return 0;
 }
 
