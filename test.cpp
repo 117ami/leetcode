0c58b7c58b54d7f378bc708e87cd7b818d80400d
GaoangLiu <ssruoz@gmail.com>
2020-08-10 13:50:17 +0800

    p1537-Hard get the maximum score [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 
 #include "aux.cpp"
-#include "1540.can-convert-string-in-k-moves.cpp"
+#include "1537.get-the-maximum-score.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	string a = "input", b="ouput"; 
-	say(s.canConvertString(a, b, 10));
+	vi na = {1,4,5,8,9,11,19}, nb={2,3,4,11,12};
+	say(s.maxSum(na, nb));
 	return 0;
 }
 
