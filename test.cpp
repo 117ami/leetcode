9276f5880e15f3b1fce9b81caaed3a9811fabe81
GaoangLiu <ssruoz@gmail.com>
2020-08-10 15:19:27 +0800

    p1536-Medium minimum swaps to arrange a binary grid [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 
 #include "aux.cpp"
-#include "1537.get-the-maximum-score.cpp"
+#include "1536.minimum-swaps-to-arrange-a-binary-grid.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vi na = {1,4,5,8,9,11,19}, nb={2,3,4,11,12};
-	say(s.maxSum(na, nb));
 	return 0;
 }
 
