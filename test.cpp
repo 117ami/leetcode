da983849e5590777058c2e1603e0a9b11416aeac
GaoangLiu <ssruoz@gmail.com>
2020-08-14 13:49:53 +0800

    p592-Medium fraction addition and subtraction [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,14 @@
 
 #include "aux.cpp"
-#include "927.three-equal-parts.cpp"
+#include "592.fraction-addition-and-subtraction.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	vector<int> x = {1, 1, 1, 0, 1, 1, 1, 1, 1, 1};
-	// x = {1,0, 1,0,1};
-	say(s.threeEqualParts(x));
+	string input = "-3/10-10/5+2/3";
+	input = "-1/2+1/2-1/3";
+	input = "1/3-1/2";
+	// input = "";
+	say(s.fractionAddition(input));
 	return 0;
 }
 
