c84c01e80c68588f2497d045913d249c4a055f63
GaoangLiu <ssruoz@gmail.com>
2020-08-18 12:00:13 +0800

    p794-Medium valid tic tac toe state [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,12 +1,9 @@
 
 #include "aux.cpp"
-#include "1053.previous-permutation-with-one-swap.cpp"
+#include "794.valid-tic-tac-toe-state.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	std::vector<int> ns = {3,1,1,3};
-	ns = {1,9,3,4,5} ;
-	say(s.prevPermOpt1(ns));
 	return 0;
 }
 
