0adf53bc91e7736bd57d680b792bfa223d8dc7ae
GaoangLiu <ssruoz@gmail.com>
2020-08-20 17:34:29 +0800

    p464-Medium can i win [C++/Python3]

--- test.cpp
+++ test.cpp
@@ -1,20 +1,10 @@
 
 #include "aux.cpp"
-#include "787.cheapest-flights-within-k-stops.cpp"
+#include "464.can-i-win.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-	std::vector<vector<int>> ns= {{0,1,100},{1,2,100}, {0,2,500}};
-	// ns = {{4,1,1},{1,2,3},{0,3,2},{0,4,10},{3,1,1},{1,4,3}};
-	// ns = {{1, 0, 5}};
-	// ns = {{0,1,1}, {1,2,1}, {0, 2, 5}, {2, 3, 1}};
-	say(s.findCheapestPrice(3, ns, 0, 2, 1));
-	// say(s.findCheapestPrice(5, ns, 2, 1, 1));
-
-	    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
-                   greater<tuple<int, int, int>>>
-        pq;
-
+	say(s.canIWin(10, 14));
 	return 0;
 }
 
