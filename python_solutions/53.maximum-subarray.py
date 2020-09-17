ab57eb7cafdfd7054688d779f79cd60be81de090
GaoangLiu <ssruoz@gmail.com>
2020-09-17 08:41:16 +0800

    p53-easy maximum subarray resolved with [C++/Python3] dp

--- python_solutions/53.maximum-subarray.py
+++ python_solutions/53.maximum-subarray.py
@@ -1,3 +1,16 @@
+from collections import Counter, defaultdict, OrderedDict, deque
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from typing import List
+import itertools
+import math
+import heapq
+import string
+true = True
+false = False
+MIN, MAX, MOD = -0x3f3f3f3f, 0x3f3f3f3f, 1000000007
+
+
 #
 # @lc app=leetcode id=53 lang=python3
 #
@@ -6,38 +19,76 @@
 # https://leetcode.com/problems/maximum-subarray/description/
 #
 # algorithms
-# Easy (45.23%)
-# Total Accepted:    714K
-# Total Submissions: 1.6M
+# Easy (46.76%)
+# Total Accepted:    1.1M
+# Total Submissions: 2.4M
 # Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
 #
 # Given an integer array nums, find the contiguous subarray (containing at
 # least one number) which has the largest sum and return its sum.
-# 
-# Example:
-# 
-# 
-# Input: [-2,1,-3,4,-1,2,1,-5,4],
+#
+# Follow up: If you have figured out the O(n) solution, try coding another
+# solution using the divide and conquer approach, which is more subtle.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 # Output: 6
-# Explanation: [4,-1,2,1] has the largest sum = 6.
-# 
-# 
-# Follow up:
-# 
-# If you have figured out the O(n) solution, try coding another solution using
-# the divide and conquer approach, which is more subtle.
-# 
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [1]
+# Output: 1
+#
+#
+# Example 3:
+#
+#
+# Input: nums = [0]
+# Output: 0
+#
+#
+# Example 4:
+#
+#
+# Input: nums = [-1]
+# Output: -1
+#
+#
+# Example 5:
+#
+#
+# Input: nums = [-2147483647]
+# Output: -2147483647
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 2 * 10^4
+# -2^31 <= nums[i] <= 2^31 - 1
+#
+#
 #
 class Solution:
-    def maxSubArray(self, nums):
-        vmax = res = nums[0]
-        for n in nums[1:]:
-            vmax = max(vmax + n, n)
-            res = max(res, vmax)
-        return res 
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        res = nums[0]
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+            res = max(res, dp[i])
+        return res
 
-s = Solution()
-arr = [-2,1,-3,4,-1,2,1,-5,4]
-print(s.maxSubArray(arr))
 
+sol = Solution()
 
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(sol.maxSubArray(nums))
--- test.cpp
+++ test.cpp
@@ -1,12 +1,20 @@
 
 #include "aux.cpp"
-#include "322.coin-change.cpp"
+#include "53.maximum-subarray.cpp"
 
 int main(int argc, char const *argv[]) {
 	Solution s;
-std::vector<int> coins = {1, 2, 5} ;
-int amount = 11 ;
-say(s.coinChange(coins, amount));
+	std::vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4} ;
+	nums={-2,1};
+	say(s.maxSubArray(nums));
+
+	// std::vector<int> nums = {1} ;
+
+	// std::vector<int> nums = {0} ;
+
+	// std::vector<int> nums = {-1} ;
+
+	// std::vector<int> nums = {-2147483647} ;
 
 
 
